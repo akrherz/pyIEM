5db2d428f945a845f398c39b38ce1d9700fc5531
akrherz <akrherz@iastate.edu>
2013-01-11 08:35:13 -0600

    remove print statement

--- src/pyiem/nws/ugc.py
+++ src/pyiem/nws/ugc.py
@@ -40,7 +40,6 @@ def parse(text, valid):
     parts = re.split('-', tokens[0][0])
     expire = str2time( tokens[0][2], valid)
     stateCode = ""
-    print tokens
     for i in range(len(parts) ):
         if i == 0:
             ugcType = parts[0][2]
