a44f70a9ff2f71dad9b33b4ec24d56c2b3a75d80
akrherz <akrherz@iastate.edu>
2015-01-20 12:56:53 -0600

    Introduce PIREP support, bump release version

---
+++ pyiem/nws/products/pirep.py
@@ -0,0 +1,194 @@
+"""
+ Parser of Pilot Reports
+"""
+import pyiem.nws.product as product
+import datetime
+import re
+import math
+from pyiem.datatypes import distance
+
+class PilotReport:
+    """ A Pilot Report Object """
+
+    def __init__(self):
+        """ Constructor"""
+        self.text = None
+        self.priority = None
+        self.latitude = None
+        self.longitude = None
+        self.valid = None
+        self.cwsu = None
+        self.aircraft_type = None
+        self.is_duplicate = False
+
+class Pirep( product.TextProduct ):
+    ''' Class for parsing and representing Space Wx Products '''
+
+    def __init__(self, text, utcnow=None, ugc_provider=None,
+                 nwsli_provider=None):
+        ''' constructor '''
+        product.TextProduct.__init__(self, text, utcnow=utcnow,
+                                     ugc_provider=ugc_provider,
+                                     nwsli_provider=nwsli_provider)
+        self.reports = []
+        self.parse_reports()
+        
+    def parse_reports(self):
+        """ Actually do the parsing of the product that generates the reports
+        stored within the self.reports list """
+        txt = self.unixtext if self.unixtext[:2] != '\001\n' else self.unixtext[2:]
+            
+        lines = txt.split("\n")
+        # There may be an AWIPSID in line 3
+        pos = 3 if len(lines[2]) < 10 else 2
+        meat = "".join(lines[pos:])
+        for report in meat.split("="):
+            if report.strip() == "":
+                continue
+            res = self.process_pirep(" ".join(report.strip().split()))
+            if res is not None:
+                self.reports.append(res)
+    
+    def process_pirep(self, report):
+        """ Convert this report text into an actual PIREP object """
+        _pr = PilotReport()
+        _pr.text = report
+        
+        for i, token in enumerate(report.split("/")):
+            token = token.strip()
+            # First token is always priority
+            if i == 0:
+                if token.find(" UUA") > 0:
+                    _pr.priority = "UUA"
+                else:
+                    _pr.priority = "UA"
+                continue
+            # Aircraft Type
+            if token.startswith("TP "):
+                _pr.aircraft_type = token[3:]
+
+            # Location
+            if token.startswith("OV "):
+                dist = 0
+                bearing = 0
+                therest = token[3:]
+                if len(therest) == 3:
+                    loc = therest
+                elif len(therest) == 4 and therest[0] == 'K':
+                    loc = therest[1:]
+                elif therest == 'O':
+                    # Use the first part of the report in this case
+                    loc = report[:3]
+                elif therest.find("-") > 0:
+                    loc = therest.split("-")[1]
+                    numbers = re.findall("[0-9]{6}", loc)
+                    if len(numbers) > 0:
+                        bearing = int(numbers[0][:3])
+                        dist = int(numbers[0][3:])
+                    loc = loc[:3]
+                else:
+                    loc = therest[:3]
+
+                if not self.nwsli_provider.has_key(loc):
+                    self.warnings.append("Unknown location: %s '%s'" % (loc,
+                                                                report))
+                    return None
+                _pr.longitude, _pr.latitude = self.compute_loc(loc, dist,
+                                                               bearing)
+                continue
+
+            # Time            
+            if token.startswith("TM "):
+                numbers = re.findall("[0-9]{4}", token)
+                if len(numbers) != 1:
+                    self.warnings.append("TM parse failed %s" % (report,))
+                    return None
+                hour = int(numbers[0][:2])
+                minute = int(numbers[0][2:])
+                _pr.valid = self.compute_pirep_valid(hour, minute)
+                continue
+        
+        return _pr
+    
+    def compute_loc(self, loc, dist, bearing):
+        """ Figure out the lon/lat for this location """
+        lat = self.nwsli_provider[loc]['lat']
+        lon = self.nwsli_provider[loc]['lon']
+        # shortcut
+        if dist == 0:
+            return lon, lat
+        meters = distance(float(dist), "MI").value("M")
+        northing = meters * math.cos(math.radians(bearing)) / 111111.0
+        easting = (meters * math.sin(math.radians(bearing)) / 
+                   math.cos(math.radians(lat)) / 111111.0)
+        #print 'meters: %.1f easting: %.3f northing: %.3f' % (meters, easting,
+        #                                               northing)
+        return lon + easting, lat + northing
+    
+    
+    def compute_pirep_valid(self, hour, minute):
+        """ Based on what utcnow is set to, compute when this is valid """
+        res = self.utcnow.replace(hour=hour, minute=minute, second=0,
+                                  microsecond=0)
+        if hour > self.utcnow.hour:
+            res -= datetime.timedelta(hours=24)
+        return res
+
+    def sql(self, txn):
+        """ Save the reports to the database via the transaction """
+        for report in self.reports:
+            if report.is_duplicate:
+                continue
+            txn.execute("""INSERT into pireps(valid, geom, is_urgent,
+            aircraft_type, report) VALUES (%s,
+            ST_GeographyFromText('SRID=4326;POINT(%s %s)'),%s,%s,%s)""", (
+            report.valid, report.longitude, report.latitude, 
+            report.priority == 'UUA',
+            report.aircraft_type, report.text))
+
+    def assign_cwsu(self, txn):
+        """ Use this transaction object to assign CWSUs for the pireps """
+        for report in self.reports:
+            sql = """select distinct id from cwsu WHERE  
+               st_contains(geom, geomFromEWKT('SRID=4326;POINT(%s %s)'))""" %(
+                report.longitude, report.latitude)
+            txn.execute(sql)
+            if txn.rowcount == 0:
+                #self.warnings.append("Find CWSU failed %.3f %.3f %s" % (
+                #    report.longitude, report.latitude, report.text))
+                continue
+            row = txn.fetchone()
+            report.cwsu = row['id']
+        
+    def get_jabbers(self, uri=None, uri2=None):
+        """ get jabber messages """
+        res = []
+        for report in self.reports:
+            if report.is_duplicate:
+                continue
+            jmsg = {'priority': 
+                    'Urgent' if report.priority == 'UUA' else 'Routine',
+                    'ts': report.valid.strftime("%H%M"),
+                    'report': report.text,
+                    'color':
+                    '#ff0000' if report.priority == 'UUA' else '#00ff00',
+             }
+            plain = "%(priority)s pilot report at %(ts)sZ: %(report)s" % jmsg
+            html = ("<span style='color:%(color)s;'>%(priority)s pilot "
+                    +"report</span> at %(ts)sZ: %(report)s") % jmsg
+            xtra = {'channels':
+                    '%s.%s,%s.PIREP' % (report.priority, report.cwsu,
+                                        report.priority),
+                    'geometry': 'POINT(%s %s)' % (report.longitude,
+                                                  report.latitude),
+                    'ptype': report.priority,
+                    'category': 'PIREP',
+                    'twitter': plain[:140],
+                    'valid': report.valid.strftime("%Y%m%dT%H:%M:00")}
+            res.append([plain, html, xtra])
+        return res
+
+def parser(buf, utcnow=None, ugc_provider=None, nwsli_provider=None):
+    ''' A parser implementation '''
+    return Pirep(buf, utcnow=utcnow, ugc_provider=ugc_provider,
+                nwsli_provider=nwsli_provider)
---
+++ pyiem/nws/products/tests/test_pirep.py
@@ -0,0 +1,60 @@
+import os
+import datetime
+import pytz
+import unittest
+from pyiem.nws.products.pirep import parser as pirepparser
+
+def get_file(name):
+    ''' Helper function to get the text file contents '''
+    basedir = os.path.dirname(__file__)
+    fn = "%s/../../../../data/product_examples/%s" % (basedir, name)
+    return open(fn).read()
+
+def utc(year, month, day, hour=0, minute=0):
+    """UTC Timestamp generator"""
+    return datetime.datetime(year, month, day, hour, minute).replace(
+                        tzinfo=pytz.timezone("UTC"))
+
+class TestProducts(unittest.TestCase):
+    """ Tests """
+
+    def test_150120_OVO(self):
+        """ PIREPS/OVO.txt has a location of OV 0 """
+        nwsli_provider = {'AVK': {'lat': 44, 'lon': 99}}
+        prod = pirepparser(get_file('PIREPS/OVO.txt'),
+                           nwsli_provider=nwsli_provider)
+        self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
+
+    def test_offset(self):
+        """ Test out our displacement logic """
+        lat = 42.5
+        lon = -92.5
+        nwsli_provider = {'BIL': {'lat': lat, 'lon': lon}}
+        p = pirepparser("\001\r\r\n000 \r\r\nUBUS01 KMSC 090000\r\r\n", 
+                        nwsli_provider=nwsli_provider)
+        lon2, lat2 = p.compute_loc("BIL", 0, 0)
+        self.assertEquals(lon2, lon)
+        self.assertEquals(lat2, lat)
+
+        lon2, lat2 = p.compute_loc("BIL", 100, 90)
+        self.assertAlmostEquals(lon2, -90.54, 2)
+        self.assertEquals(lat2, lat)
+
+        lon2, lat2 = p.compute_loc("BIL", 100, 0)
+        self.assertEquals(lon2, lon)
+        self.assertAlmostEquals(lat2, 43.95, 2)
+
+
+    def test_1(self):
+        """ PIREP.txt, can we parse it! """
+        utcnow = utc(2015,1,9,0,0)
+        nwsli_provider = {'BIL': {'lat': 44, 'lon': 99},
+                          'LBY': {'lat': 45, 'lon': 100},
+                          'PUB': {'lat': 46, 'lon': 101},
+                          'HPW': {'lat': 47, 'lon': 102}}
+        prod = pirepparser(get_file('PIREP.txt'), utcnow=utcnow,
+                           nwsli_provider=nwsli_provider)
+        self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
+        
+        j = prod.get_jabbers()
+        self.assertEquals(j[0][2]['channels'], 'UA.None,UA.PIREP')
