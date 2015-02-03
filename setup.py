79bd65b7b8d547a2097c7f7bd6695e438e5730a1
akrherz <akrherz@iastate.edu>
2015-02-03 08:06:06 -0600

    Add windrose package to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version=pyiem.__version__,
     author='daryl herzmann',
     author_email='akrherz@gmail.com',
-    packages=['pyiem', 'pyiem.nws', 'pyiem.nws.products'],
+    packages=['pyiem', 'pyiem.nws', 'pyiem.nws.products', 'pyiem.windrose'],
     package_data={'pyiem': ['data/*',]},
     url='https://github.com/akrherz/pyIEM/',
     license='Apache',
