94342415c6a4923911af376f6123d88e43c02ed7
akrherz <akrherz@iastate.edu>
2015-07-15 09:18:32 -0500

    pep8 fixes for datatypes

--- pyiem/datatypes.py
+++ pyiem/datatypes.py
@@ -3,9 +3,11 @@
 """
 import numpy as np
 
+
 class UnitsError(Exception):
     """ Exception for bad Units """
     pass
+
 
 class basetype(object):
     """ Base Object for all our vars """
@@ -16,33 +18,35 @@ class basetype(object):
             raise UnitsError("unrecognized temperature unit: %s known: %s" % (
                                 units, self.known_units))
         self._units = units.upper()
-        if type(value) == type([]):
+        if isinstance(value, list):
             self._value = np.array(value)
         else:
             self._value = value
 
+
 class mixingratio(basetype):
     known_units = ["KG/KG", ]
-    
+
     def value(self, units):
         """ Convert the value into the provided units """
         if units.upper() not in mixingratio.known_units:
             raise UnitsError("unrecognized mixingratio unit: %s known: %s" % (
                                 units, mixingratio.known_units))
         if units.upper() == self._units:
-            return self._value   
+            return self._value
+
 
 class distance(basetype):
     """ Distance """
     known_units = ["SM", "MI", "M", "KM", "FT", "MM", "IN", "CM"]
-    
+
     def value(self, units):
         """ Convert to a value in the given units """
         if units.upper() not in distance.known_units:
             raise UnitsError("unrecognized distance unit: %s known: %s" % (
                                 units, distance.known_units))
         if units.upper() == self._units:
-            return self._value   
+            return self._value
 
         # MKS
         if self._units in ["SM", "MI"]:
@@ -82,11 +86,11 @@ class precipitation(basetype):
     def value(self, units):
         """ Convert to a value in the given units """
         if units.upper() not in precipitation.known_units:
-            raise UnitsError("unrecognized precipitation unit: %s known: %s" % (
-                                units, precipitation.known_units))
+            raise UnitsError(("unrecognized precipitation unit: %s known: %s"
+                              ) % (units, precipitation.known_units))
         if units.upper() == self._units:
             return self._value
-        
+
         # MKS
         if self._units == 'IN':
             mm = self._value * 25.4
@@ -94,7 +98,7 @@ class precipitation(basetype):
             mm = self._value * 10.0
         else:
             mm = self._value
-            
+
         # Convert
         if units == 'MM':
             return mm
@@ -102,7 +106,8 @@ class precipitation(basetype):
             return mm * 10.0
         elif units == 'IN':
             return mm / 25.4
-        
+
+
 class speed(basetype):
     """ Speed """
     known_units = ['KT', 'MPH', 'MPS', 'KMH']
@@ -135,9 +140,10 @@ class speed(basetype):
         elif units == "MPS":
             return mps_value
 
+
 class humidity(basetype):
     """ Humidity, this is not as straight forward as the others """
-    known_units = ['%',]
+    known_units = ['%', ]
 
     def value(self, units):
         """ Convert to a value in the given units """
@@ -145,6 +151,7 @@ class humidity(basetype):
             raise UnitsError("unrecognized humidity unit: %s known: %s" % (
                                 units, humidity.known_units))
         return self._value
+
 
 class direction(basetype):
     """ Direction from North """
@@ -157,11 +164,12 @@ class direction(basetype):
                                 units, direction.known_units))
         if units.upper() == self._units:
             return self._value
-        
+
         if self._units == "DEG" and units.upper() == 'RAD':
             return self._value * 3.1415926535897931 / 180.0
         if self._units == "RAD" and units.upper() == 'DEG':
-            return self._value * 180 / 3.1415926535897931 
+            return self._value * 180 / 3.1415926535897931
+
 
 class pressure(basetype):
     """ Pressure """
@@ -174,14 +182,14 @@ class pressure(basetype):
                                 units, pressure.known_units))
         if units.upper() == self._units:
             return self._value
-        
+
         # MKS
         if self._units == "IN":
             mb_value = self._value * 33.86398
         else:
             mb_value = self._value
         # Now convert
-        if units.upper() in ["MB","HPA"]:
+        if units.upper() in ["MB", "HPA"]:
             return mb_value
         elif units.upper() == 'PA':
             return mb_value * 100.0
@@ -192,7 +200,7 @@ class pressure(basetype):
 class temperature(basetype):
     """ Temperature """
     known_units = ['F', 'K', 'C']
-            
+
     def value(self, units):
         """ Convert to a value in the given units """
         if units.upper() not in temperature.known_units:
@@ -200,12 +208,12 @@ class temperature(basetype):
                                 units, temperature.known_units))
         if units.upper() == self._units:
             return self._value
-        
+
         # Convert to Celsius first
         if self._units == 'C':
             celsius_value = self._value
         elif self._units == 'F':
-            celsius_value = (self._value -32.0) / 1.8
+            celsius_value = (self._value - 32.0) / 1.8
         elif self._units == 'K':
             celsius_value = self._value - 273.15
         # Dump back
