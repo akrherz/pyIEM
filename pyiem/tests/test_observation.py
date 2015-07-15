558938c79c18db9e62be68d912b42dae82c60ea9
akrherz <akrherz@iastate.edu>
2015-07-15 08:52:55 -0500

    remove unused/hacky iemdb lib

--- pyiem/tests/test_observation.py
+++ pyiem/tests/test_observation.py
@@ -1,8 +1,10 @@
 import unittest
 import datetime
 import pytz
+import psycopg2.extras
 
-from pyiem import observation, iemdb
+from pyiem import observation
+
 
 class TestObservation(unittest.TestCase):
 
@@ -10,17 +12,15 @@ class TestObservation(unittest.TestCase):
         ts = datetime.datetime.utcnow()
         ts = ts.replace(tzinfo=pytz.timezone("UTC"))
         self.ob = observation.Observation('DSM', 'IA_ASOS', ts)
-        (self.conn, self.cursor) = iemdb.cnc('iem', host='127.0.0.1')
-        
+        self.conn = psycopg2.connect(database='iem', host='iemdb')
+        self.cursor = self.conn.cursor(
+                        cursor_factory=psycopg2.extras.DictCursor)
+
     def test_simple(self):
         """ Simple check of observation """
         self.assertTrue(self.ob)
-        
+
     def test_update(self):
         """ Make sure we can update the database """
-        updated = self.ob.save(self.cursor)
+        _ = self.ob.save(self.cursor)
         self.assertTrue(True)
-
-
-if __name__ == '__main__':
-    unittest.main()
