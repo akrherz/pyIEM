3ea5e222438a00d61f94d531d2ec1284399f9403
akrherz <akrherz@iastate.edu>
2015-07-16 14:54:46 -0500

    bundle a small Hawaii GOES product to ensure tests are run against it

--- pyiem/nws/tests/test_gini.py
+++ pyiem/nws/tests/test_gini.py
@@ -3,15 +3,19 @@ from pyiem.nws import gini
 import os
 
 
+def get_filepath(name):
+    ''' Helper function to get the text file contents '''
+    basedir = os.path.dirname(__file__)
+    return "%s/../../../data/product_examples/%s" % (basedir, name)
+
+
 class TestGINI(unittest.TestCase):
 
     def test_gini(self):
         """ check GINI Processing of Goes East VIS parsing """
-        # This fn may not be available
-        fn = "data/product_examples/TIGE01"
-        if not os.path.isfile(fn):
-            return
+        fn = get_filepath("TIGH05")
         sat = gini.GINIZFile(open(fn))
-
-        self.assertEqual(sat.archive_filename(),
-                         "GOES_EAST_VIS_201301111601.png")
+        self.assertEqual(sat.archive_filename(), "GOES_HI_WV_201507161745.png")
+        self.assertEqual(str(sat),
+                         "TIGH05 KNES 161745 Line Size: 560 Num Lines: 520")
+        self.assertEqual(sat.awips_grid(), 208)
