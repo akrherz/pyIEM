6cd7547109262ce0fa97bf5d61cd8153bacfbcd8
akrherz <akrherz@iastate.edu>
2015-07-20 10:02:32 -0500

    pep8 fixes, increase coverage of plot.py, bundle iem logo

--- pyiem/plot.py
+++ pyiem/plot.py
@@ -194,11 +194,12 @@ def load_bounds(filename):
     """
     return np.load("%s/%s.npy" % (DATADIR, filename))
 
+
 def mask_outside_polygon(poly_verts, ax=None):
     """
     We produce a polygon that lies between the plot border and some interior
-    polygon.  
-    
+    polygon.
+
     POLY_VERTS is in CCW order, as this is the interior of the polygon
     """
     import matplotlib.patches as mpatches
@@ -256,47 +257,48 @@ def james():
     cm.register_cmap(cmap=cmap3)
     return cmap3
 
+
 def whitebluegreenyellowred():
     ''' Rip off NCL's WhiteBlueGreenYellowRed '''
-    cpool = ['#cfedfb', '#cdecfb', '#caebfb', '#c7eafa', '#c5e9fa', '#c2e8fa', 
-             '#bfe7fa', '#bde6fa', '#bae5f9', '#b7e4f9', '#b5e3f9', '#b2e2f9', 
-             '#b0e1f9', '#ade0f8', '#aadff8', '#a8def8', '#a5ddf8', '#a2dcf7', 
-             '#9ddaf7', '#9bd8f6', '#98d6f5', '#96d4f3', '#94d2f2', '#92d0f1', 
-             '#8fcef0', '#8dccee', '#8bcaed', '#88c8ec', '#86c5eb', '#84c3ea', 
-             '#81c1e8', '#7fbfe7', '#7dbde6', '#7bbbe5', '#78b9e4', '#76b7e2', 
-             '#74b5e1', '#71b3e0', '#6fb1df', '#6dafdd', '#6aaddc', '#68abdb', 
-             '#66a9da', '#64a7d9', '#61a5d7', '#5fa3d6', '#5da0d5', '#5a9ed4', 
-             '#589cd3', '#569ad1', '#5398d0', '#5196cf', '#4f94ce', '#4d92cc', 
-             '#488eca', '#488fc6', '#4890c3', '#4891bf', '#4892bc', '#4893b8', 
-             '#4894b5', '#4895b1', '#4896ad', '#4897aa', '#4899a6', '#489aa3', 
-             '#489b9f', '#489c9c', '#489d98', '#489e94', '#489f91', '#48a08d', 
-             '#48a18a', '#49a286', '#49a383', '#49a47f', '#49a57c', '#49a678', 
-             '#49a774', '#49a871', '#49a96d', '#49aa6a', '#49ac66', '#49ad63', 
-             '#49ae5f', '#49af5b', '#49b058', '#49b154', '#49b251', '#49b34d', 
-             '#49b546', '#4eb647', '#53b847', '#57b948', '#5cbb48', '#61bc49', 
-             '#66bd4a', '#6abf4a', '#6fc04b', '#74c14b', '#79c34c', '#7ec44d', 
-             '#82c64d', '#87c74e', '#8cc84e', '#91ca4f', '#96cb50', '#9acc50', 
-             '#9fce51', '#a4cf51', '#a9d152', '#add252', '#b2d353', '#b7d554', 
-             '#bcd654', '#c1d755', '#c5d955', '#cada56', '#cfdc57', '#d4dd57', 
-             '#d9de58', '#dde058', '#e2e159', '#e7e25a', '#ece45a', '#f0e55b', 
-             '#f5e75b', '#fae85c', '#fae55b', '#fae159', '#fade58', '#f9da56', 
-             '#f9d755', '#f9d454', '#f9d052', '#f9cd51', '#f9c950', '#f9c64e', 
-             '#f9c34d', '#f8bf4b', '#f8bc4a', '#f8b849', '#f8b547', '#f8b246', 
-             '#f8ae45', '#f8ab43', '#f7a742', '#f7a440', '#f7a03f', '#f79d3e', 
-             '#f79a3c', '#f7963b', '#f7933a', '#f68f38', '#f68c37', '#f68935', 
-             '#f68534', '#f68233', '#f67e31', '#f67b30', '#f6782f', '#f5742d', 
-             '#f5712c', '#f56a29', '#f46829', '#f36629', '#f26429', '#f16229', 
-             '#f06029', '#ef5e29', '#ef5c29', '#ee5a29', '#ed5829', '#ec5629', 
-             '#eb5429', '#ea5229', '#e95029', '#e84e29', '#e74c29', '#e64a29', 
-             '#e54829', '#e44629', '#e44328', '#e34128', '#e23f28', '#e13d28', 
-             '#e03b28', '#df3928', '#de3728', '#dd3528', '#dc3328', '#db3128', 
-             '#da2f28', '#d92d28', '#d92b28', '#d82928', '#d72728', '#d62528', 
-             '#d52328', '#d31f28', '#d11f28', '#cf1e27', '#ce1e27', '#cc1e26', 
-             '#ca1e26', '#c81d26', '#c71d25', '#c51d25', '#c31d24', '#c11c24', 
-             '#c01c24', '#be1c23', '#bc1b23', '#ba1b22', '#b91b22', '#b71b22', 
-             '#b51a21', '#b31a21', '#b21a20', '#b01a20', '#ae191f', '#ac191f', 
-             '#ab191f', '#a9191e', '#a7181e', '#a5181d', '#a4181d', '#a2171d', 
-             '#a0171c', '#9e171c', '#9d171b', '#9b161b', '#99161b', '#97161a', 
+    cpool = ['#cfedfb', '#cdecfb', '#caebfb', '#c7eafa', '#c5e9fa', '#c2e8fa',
+             '#bfe7fa', '#bde6fa', '#bae5f9', '#b7e4f9', '#b5e3f9', '#b2e2f9',
+             '#b0e1f9', '#ade0f8', '#aadff8', '#a8def8', '#a5ddf8', '#a2dcf7',
+             '#9ddaf7', '#9bd8f6', '#98d6f5', '#96d4f3', '#94d2f2', '#92d0f1',
+             '#8fcef0', '#8dccee', '#8bcaed', '#88c8ec', '#86c5eb', '#84c3ea',
+             '#81c1e8', '#7fbfe7', '#7dbde6', '#7bbbe5', '#78b9e4', '#76b7e2',
+             '#74b5e1', '#71b3e0', '#6fb1df', '#6dafdd', '#6aaddc', '#68abdb',
+             '#66a9da', '#64a7d9', '#61a5d7', '#5fa3d6', '#5da0d5', '#5a9ed4',
+             '#589cd3', '#569ad1', '#5398d0', '#5196cf', '#4f94ce', '#4d92cc',
+             '#488eca', '#488fc6', '#4890c3', '#4891bf', '#4892bc', '#4893b8',
+             '#4894b5', '#4895b1', '#4896ad', '#4897aa', '#4899a6', '#489aa3',
+             '#489b9f', '#489c9c', '#489d98', '#489e94', '#489f91', '#48a08d',
+             '#48a18a', '#49a286', '#49a383', '#49a47f', '#49a57c', '#49a678',
+             '#49a774', '#49a871', '#49a96d', '#49aa6a', '#49ac66', '#49ad63',
+             '#49ae5f', '#49af5b', '#49b058', '#49b154', '#49b251', '#49b34d',
+             '#49b546', '#4eb647', '#53b847', '#57b948', '#5cbb48', '#61bc49',
+             '#66bd4a', '#6abf4a', '#6fc04b', '#74c14b', '#79c34c', '#7ec44d',
+             '#82c64d', '#87c74e', '#8cc84e', '#91ca4f', '#96cb50', '#9acc50',
+             '#9fce51', '#a4cf51', '#a9d152', '#add252', '#b2d353', '#b7d554',
+             '#bcd654', '#c1d755', '#c5d955', '#cada56', '#cfdc57', '#d4dd57',
+             '#d9de58', '#dde058', '#e2e159', '#e7e25a', '#ece45a', '#f0e55b',
+             '#f5e75b', '#fae85c', '#fae55b', '#fae159', '#fade58', '#f9da56',
+             '#f9d755', '#f9d454', '#f9d052', '#f9cd51', '#f9c950', '#f9c64e',
+             '#f9c34d', '#f8bf4b', '#f8bc4a', '#f8b849', '#f8b547', '#f8b246',
+             '#f8ae45', '#f8ab43', '#f7a742', '#f7a440', '#f7a03f', '#f79d3e',
+             '#f79a3c', '#f7963b', '#f7933a', '#f68f38', '#f68c37', '#f68935',
+             '#f68534', '#f68233', '#f67e31', '#f67b30', '#f6782f', '#f5742d',
+             '#f5712c', '#f56a29', '#f46829', '#f36629', '#f26429', '#f16229',
+             '#f06029', '#ef5e29', '#ef5c29', '#ee5a29', '#ed5829', '#ec5629',
+             '#eb5429', '#ea5229', '#e95029', '#e84e29', '#e74c29', '#e64a29',
+             '#e54829', '#e44629', '#e44328', '#e34128', '#e23f28', '#e13d28',
+             '#e03b28', '#df3928', '#de3728', '#dd3528', '#dc3328', '#db3128',
+             '#da2f28', '#d92d28', '#d92b28', '#d82928', '#d72728', '#d62528',
+             '#d52328', '#d31f28', '#d11f28', '#cf1e27', '#ce1e27', '#cc1e26',
+             '#ca1e26', '#c81d26', '#c71d25', '#c51d25', '#c31d24', '#c11c24',
+             '#c01c24', '#be1c23', '#bc1b23', '#ba1b22', '#b91b22', '#b71b22',
+             '#b51a21', '#b31a21', '#b21a20', '#b01a20', '#ae191f', '#ac191f',
+             '#ab191f', '#a9191e', '#a7181e', '#a5181d', '#a4181d', '#a2171d',
+             '#a0171c', '#9e171c', '#9d171b', '#9b161b', '#99161b', '#97161a',
              '#96161a', '#921519']
     cmap3 = mpcolors.ListedColormap(cpool, 'whitebluegreenyellowred')
     cmap3.set_over("#000000")
@@ -305,16 +307,17 @@ def whitebluegreenyellowred():
     cm.register_cmap(cmap=cmap3)
     return cmap3
 
+
 def maue():
     """ Pretty color ramp Dr Ryan Maue uses """
     cpool = ["#e6e6e6", "#d2d2d2", "#bcbcbc", "#969696", "#646464",
-             "#1464d2", "#1e6eeb", "#2882f0", "#3c96f5", "#50a5f5", "#78b9fa", 
+             "#1464d2", "#1e6eeb", "#2882f0", "#3c96f5", "#50a5f5", "#78b9fa",
              "#96d2fa", "#b4f0fa", "#e1ffff",
-             "#0fa00f", "#1eb41e", "#37d23c", "#50f050", "#78f573", "#96f58c", 
+             "#0fa00f", "#1eb41e", "#37d23c", "#50f050", "#78f573", "#96f58c",
              "#b4faaa", "#c8ffbe",
-             "#ffe878", "#ffc03c", "#ffa000", "#ff6000", "#ff3200", "#e11400", 
+             "#ffe878", "#ffc03c", "#ffa000", "#ff6000", "#ff3200", "#e11400",
              "#c00000", "#a50000", "#643c32",
-             "#785046", "#8c645a", "#b48c82", "#e1beb4", "#f0dcd2", "#ffc8c8", 
+             "#785046", "#8c645a", "#b48c82", "#e1beb4", "#f0dcd2", "#ffc8c8",
              "#f5a0a0", "#f5a0a0", "#e16464", "#c83c3c"]
 
     cmap3 = mpcolors.ListedColormap(cpool, 'maue')
@@ -352,7 +355,7 @@ class MapPlot(object):
         self.ax = plt.axes([0.01, 0.05, 0.928, 0.85],
                            axisbg=(0.4471, 0.6235, 0.8117))
         self.cax = plt.axes([0.941, 0.1, 0.058, 0.8], frameon=False,
-                      yticks=[], xticks=[])
+                            yticks=[], xticks=[])
         self.sector = sector
         self.ak_map = None
         self.ak_ax = None
@@ -1082,12 +1085,12 @@ class MapPlot(object):
 
     def iemlogo(self):
         """ Draw a logo """
-        fn = '/mesonet/www/apps/iemwebsite/htdocs/images/logo_small.png'
+        fn = '%s/%s' % (DATADIR, 'logo.png')
         if not os.path.isfile(fn):
             return
         logo = Image.open(fn)
-        ax3 = plt.axes([0.02,0.89,0.1,0.1], frameon=False, 
-                       axisbg=(0.4471,0.6235,0.8117), yticks=[], xticks=[])
+        ax3 = plt.axes([0.02, 0.89, 0.1, 0.1], frameon=False,
+                       axisbg=(0.4471, 0.6235, 0.8117), yticks=[], xticks=[])
         ax3.imshow(logo, origin='upper')
 
     def make_colorbar(self, bins, colorramp):
@@ -1149,12 +1152,12 @@ class MapPlot(object):
         os.unlink(tmpfn)
 
 
-def windrose(station, database='asos', fp=None, months=np.arange(1,13),
-    hours=np.arange(0,24), sts=datetime.datetime(1970,1,1),
-    ets=datetime.datetime(2050,1,1), units="mph", nsector=36,
-    justdata=False):
+def windrose(station, database='asos', fp=None, months=np.arange(1, 13),
+             hours=np.arange(0, 24), sts=datetime.datetime(1970, 1, 1),
+             ets=datetime.datetime(2050, 1, 1), units="mph", nsector=36,
+             justdata=False):
     """Utility function that generates a windrose plot
-    
+
     Args:
       station (str): station identifier to search database for
       database (str,optional): database name to look for data within
@@ -1169,27 +1172,31 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
       justdata (boolean,optional): if True, write out the data only
     """
     from windrose import WindroseAxes
+    from windrose.windrose import histogram
     windunits = {
         'mph': {'label': 'miles per hour', 'dbmul': 1.15,
-                'bins':(0,2,5,7,10,15,20), 'abbr': 'mph',
-                'binlbl':('2-5','5-7','7-10','10-15','15-20','20+')},
+                'bins': (0, 2, 5, 7, 10, 15, 20), 'abbr': 'mph',
+                'binlbl': ('2-5', '5-7', '7-10', '10-15', '15-20', '20+')},
         'kts': {'label': 'knots', 'dbmul': 1.0,
-                'bins':(0,2,5,7,10,15,20), 'abbr': 'kts',
-                'binlbl':('2-5','5-7','7-10','10-15','15-20','20+')},
+                'bins': (0, 2, 5, 7, 10, 15, 20), 'abbr': 'kts',
+                'binlbl': ('2-5', '5-7', '7-10', '10-15', '15-20', '20+')},
         'mps': {'label': 'meters per second', 'dbmul': 0.5144,
-                'bins':(0,2,4,6,8,10,12), 'abbr': 'm s$^{-1}$',
-                'binlbl':('2-4','4-6','6-8','8-10','10-12','12+')},    
+                'bins': (0, 2, 4, 6, 8, 10, 12), 'abbr': 'm s$^{-1}$',
+                'binlbl': ('2-4', '4-6', '6-8', '8-10', '10-12', '12+')},
         'kph': {'label': 'kilometers per hour', 'dbmul': 1.609,
-                'bins':(0,4,10,14,20,30,40), 'abbr': '$km h^{-1}$',
-                'binlbl':('4-10','10-14','14-20','20-30','30-40','40+')},              
+                'bins': (0, 4, 10, 14, 20, 30, 40), 'abbr': '$km h^{-1}$',
+                'binlbl': ('4-10', '10-14', '14-20', '20-30', '30-40', '40+')},
     }
-    
+
     # Query metadata
     db = psycopg2.connect(database='mesosite', host='iemdb', user='nobody')
     mcursor = db.cursor()
-    mcursor.execute("""SELECT name from stations where id = %s""" ,(station,))
+    mcursor.execute("""SELECT name from stations where id = %s""", (station,))
     row = mcursor.fetchall()
-    sname = row[0][0]
+    if row is None:
+        sname = "((%s))" % (station,)
+    else:
+        sname = row[0][0]
     mcursor.close()
     db.close()
 
@@ -1200,9 +1207,8 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
         month_limit_text = str(tuple(months))
     elif len(months) < 12:
         monthLimiter = "and extract(month from valid) in %s" % (
-                                    (str(tuple(months))).replace("'",""),)
+                                    (str(tuple(months))).replace("'", ""),)
         month_limit_text = str(tuple(months))
-
 
     hour_limit_text = "All included"
     hourLimiter = ""
@@ -1211,7 +1217,7 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
         hour_limit_text = str(tuple(hours))
     elif len(hours) < 24:
         hourLimiter = "and extract(hour from valid) in %s" % (
-                                    (str(tuple(hours))).replace("'",""),)
+                                    (str(tuple(hours))).replace("'", ""),)
         hour_limit_text = str(tuple(hours))
 
     # Query observations
@@ -1220,14 +1226,13 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
     sql = """SELECT sknt, drct, valid from alldata WHERE station = '%s'
         and valid > '%s' and valid < '%s'
         %s
-        %s """ % (
-        station, sts, ets, monthLimiter, hourLimiter)
-    acursor.execute( sql )
-    sped = np.zeros( (acursor.rowcount,), 'f')
-    drct = np.zeros( (acursor.rowcount,), 'f')
+        %s """ % (station, sts, ets, monthLimiter, hourLimiter)
+    acursor.execute(sql)
+    sped = np.zeros((acursor.rowcount,), 'f')
+    drct = np.zeros((acursor.rowcount,), 'f')
     i = 0
     for row in acursor:
-        #if row[2].month not in months or row[2].hour not in hours:
+        # if row[2].month not in months or row[2].hour not in hours:
         #    continue
         if i == 0:
             minvalid = row[2]
@@ -1243,8 +1248,8 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
             sped[i] = 0
             drct[i] = 0
         else:
-            sped[i] =  row[0] * windunits[units]['dbmul'] 
-            drct[i] =  row[1] 
+            sped[i] = row[0] * windunits[units]['dbmul']
+            drct[i] = row[1]
         i += 1
 
     acursor.close()
@@ -1252,17 +1257,17 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
     if i < 5 or max(sped) == 0:
         _ = plt.figure(figsize=(6, 7), dpi=80, facecolor='w', edgecolor='w')
         label = 'Not enough data available to generate plot'
-        plt.gcf().text(0.17,0.89, label)
+        plt.gcf().text(0.17, 0.89, label)
         if fp is not None:
             plt.savefig(fp)
         else:
             print "Content-Type: image/png\n"
-            plt.savefig( sys.stdout, format='png' )
+            plt.savefig(sys.stdout, format='png')
         return
 
     if justdata:
         sys.stdout.write('Content-type: text/plain\n\n')
-        dir_edges, var_bins, table = wr.histogram(drct, sped, 
+        dir_edges, var_bins, table = histogram(drct, sped,
                                         np.asarray(windunits[units]['bins']), 
                                         nsector, normed=True)
         sys.stdout.write(("# Windrose Data Table (Percent Frequency) "
@@ -1303,9 +1308,9 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
         color = p.get_facecolor()
         handles.append( plt.Rectangle((0, 0), 0.1, 0.3,
                     facecolor=color, edgecolor='black'))
-    l = fig.legend( handles, windunits[units]['binlbl'] , loc=3,
-     ncol=6, title='Wind Speed [%s]' % (windunits[units]['abbr'],), 
-     mode=None, columnspacing=0.9, handletextpad=0.45)
+    l = fig.legend(handles, windunits[units]['binlbl'] , loc=3,
+                   ncol=6, title='Wind Speed [%s]' % (windunits[units]['abbr'],), 
+                   mode=None, columnspacing=0.9, handletextpad=0.45)
     plt.setp(l.get_texts(), fontsize=10)
     # Now we put some fancy debugging info on the plot
     tlimit = "Time Domain: "
@@ -1314,32 +1319,30 @@ def windrose(station, database='asos', fp=None, months=np.arange(1,13),
     if len(hours) < 24:
         if len(hours) > 4:
             tlimit += "%s-%s" % (
-                    datetime.datetime(2000,1,1,hours[0]).strftime("%-I %p"),
-                    datetime.datetime(2000,1,1,hours[-1]).strftime("%-I %p")                                 
+                    datetime.datetime(2000, 1, 1, hours[0]).strftime("%-I %p"),
+                    datetime.datetime(2000, 1, 1, hours[-1]).strftime("%-I %p")
                                  )
         else:
-            for h in hours: 
+            for h in hours:
                 tlimit += "%s," % (
-                            datetime.datetime(2000,1,1,h).strftime("%-I %p"),)
+                            datetime.datetime(2000, 1, 1, h).strftime("%-I %p"),)
     if len(months) < 12:
-        for h in months: 
+        for h in months:
             tlimit += "%s," % (datetime.datetime(2000,h,1).strftime("%b"),)
-    label = """[%s] %s  
+    label = """[%s] %s
 Windrose Plot [%s]
 Period of Record: %s - %s
-Obs Count: %s Calm: %.1f%% Avg Speed: %.1f %s""" % (station, sname, 
-                                                             tlimit,
-        minvalid.strftime("%d %b %Y"), maxvalid.strftime("%d %b %Y"), 
+Obs Count: %s Calm: %.1f%% Avg Speed: %.1f %s""" % (station, sname, tlimit,
+                                                    minvalid.strftime("%d %b %Y"), maxvalid.strftime("%d %b %Y"), 
         np.shape(sped)[0], 
         np.sum( np.where(sped < 2., 1., 0.)) / np.shape(sped)[0] * 100.,
         np.average(sped), windunits[units]['abbr'])
-    plt.gcf().text(0.17,0.89, label)
-    plt.gcf().text(0.01,0.1, "Generated: %s" % (
+    plt.gcf().text(0.17, 0.89, label)
+    plt.gcf().text(0.01, 0.1, "Generated: %s" % (
                    datetime.datetime.now().strftime("%d %b %Y"),),
                    verticalalignment="bottom")
     # Make a logo
-    im = mpimage.imread('/mesonet/www/apps/iemwebsite/htdocs/images/logo_small.png')
-    #im[:,:,-1] = 0.8
+    im = mpimage.imread('%s/%s' % (DATADIR, 'logo.png'))
 
     plt.figimage(im, 10, 625)
 
@@ -1347,6 +1350,6 @@ Obs Count: %s Calm: %.1f%% Avg Speed: %.1f %s""" % (station, sname,
         plt.savefig(fp)
     else:
         sys.stdout.write("Content-Type: image/png\n\n")
-        plt.savefig( sys.stdout, format='png' )
-   
+        plt.savefig(sys.stdout, format='png')
+
     del sped, drct, im
--- pyiem/tests/test_plot.py
+++ pyiem/tests/test_plot.py
@@ -1,9 +1,33 @@
 import unittest
 import numpy as np
 from pyiem import plot
+import datetime
 
 
 class TestPlot(unittest.TestCase):
+
+    def test_windrose(self):
+        """Exercise the windrose code"""
+        plot.windrose('AMW')
+        self.assertEquals(1, 1)
+
+    def test_colorramps(self):
+        """make sure our colorramps are happy"""
+        c = plot.james()
+        self.assertEquals(c.N, 12)
+        c = plot.james2()
+        self.assertEquals(c.N, 12)
+        c = plot.whitebluegreenyellowred()
+        self.assertEquals(c.N, 236)
+
+    def test_calendar(self):
+        """See if we can make a calendar plot!"""
+        import matplotlib.pyplot as plt
+        sts = datetime.date(2015, 5, 1)
+        ets = datetime.date(2015, 7, 1)
+        data = dict()
+        fig = plot.calendar_plot(sts, ets, data)
+        self.assertTrue(isinstance(fig, plt.Figure))
 
     def test_overlap(self):
         """ Do some checking of our overlaps logic """
@@ -29,28 +53,28 @@ class TestPlot(unittest.TestCase):
     def test_scatter(self):
         """ Test scatter plots """
         m = plot.MapPlot(sector='midwest')
-        m.scatter(np.linspace(-99,-94, 100), 
-                  np.linspace(40,45,100), range(100), np.arange(0,101,10))
+        m.scatter(np.linspace(-99, -94, 100),
+                  np.linspace(40, 45, 100), range(100), np.arange(0, 101, 10))
         m.postprocess(filename='/tmp/test_plot_scatter.png')
         m.close()
 
     def test_contourf(self):
         ''' Test the contourf plot with labels specified '''
         m = plot.MapPlot(sector='iowa')
-        m.contourf(range(-94,-89), range(40,45), range(5), range(5),
-                   clevlabels=['a','b', 'c', 'd', 'e'])
+        m.contourf(range(-94, -89), range(40, 45), range(5), range(5),
+                   clevlabels=['a', 'b', 'c', 'd', 'e'])
         m.postprocess(filename='/tmp/test_plot_contourf.png')
         m.close()
 
     def test_textplot(self):
         ''' Can we plot text and place labels on them '''
         m = plot.MapPlot(sector='iowa')
-        m.plot_values(range(-99,-94), range(40,45), range(5))
+        m.plot_values(range(-99, -94), range(40, 45), range(5))
         m.postprocess(filename='/tmp/test_plot_1.png')
 
         m = plot.MapPlot(sector='iowa')
-        m.plot_values(range(-99,-94), range(40,45), range(5), 
-                      labels=range(5,11))
+        m.plot_values(range(-99, -94), range(40, 45), range(5),
+                      labels=range(5, 11))
         m.postprocess(filename='/tmp/test_plot_2.png')
         m.close()
 
