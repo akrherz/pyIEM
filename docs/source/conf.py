c2050632c1ecd534f3ee927c18aeb505d14ec9ba
akrherz <akrherz@iastate.edu>
2015-07-22 18:19:53 -0500

    attempt to make rtd happy again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,11 +21,11 @@ import sys, os
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
