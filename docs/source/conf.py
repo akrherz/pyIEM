a327c0c0863f6906b8860d8e3f6b1ecc3c14505a
akrherz <akrherz@iastate.edu>
2015-08-21 15:16:17 -0500

    more mocking fun for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -250,5 +250,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['mpl_toolkits', 'shapely']
+MOCK_MODULES = ['mpl_toolkits.basemap', 'shapely.geometry']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
