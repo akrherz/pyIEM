d543ba8fe5eb4fc3a6b22c30a4dda60b227d6c94
akrherz <akrherz@iastate.edu>
2015-08-21 15:07:50 -0500

    mock shapely

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -250,5 +250,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['mpl_toolkits']
+MOCK_MODULES = ['mpl_toolkits', 'shapely']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
