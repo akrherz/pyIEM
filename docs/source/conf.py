aaf92cfd6e94f1118be9caa58d355f3c53779c98
akrherz <akrherz@iastate.edu>
2015-08-21 14:19:57 -0500

    attempt to mock out requirements, so to make RTD happy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,18 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['mpl_toolkits', 'basemap', 'matplotlib', 'numpy',
+                'scipy', 'PIL', 'psycopg2', 'shapely', 'pyproj',
+                'metar', 'windrose']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
