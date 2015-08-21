ca30f23a3a492d601eca6d59bcfaf265204db25f
akrherz <akrherz@iastate.edu>
2015-08-21 15:02:18 -0500

    another napolean hack for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
