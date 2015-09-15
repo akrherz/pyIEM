5d6df4dd55087ae157135bff1f62b265d359844a
akrherz <akrherz@iastate.edu>
2015-09-15 11:34:08 -0500

    increase sigmet coverage and support more types

--- pyiem/nws/products/sigmet.py
+++ pyiem/nws/products/sigmet.py
@@ -10,15 +10,18 @@ from shapely.geometry import Polygon, Point
 # Local stuff
 from pyiem.nws.product import TextProduct
 
-O_LINE1 = re.compile("SIGMET (?P<name>[A-Z]*) (?P<num>[0-9]*) VALID (?P<sts>[0-9]{6})/(?P<ets>[0-9]{6})")
-O_PAIRS = re.compile("(?P<lat>[NS][0-9]{4}) (?P<lon>[EW][0-9]{5})")
+O_LINE1 = re.compile(("SIGMET (?P<name>[A-Z]*) (?P<num>[0-9]*) "
+                      "VALID (?P<sts>[0-9]{6})/(?P<ets>[0-9]{6})"))
+
+O_PAIRS = re.compile("(?P<lat>[NS][0-9]{4})\s?(?P<lon>[EW][0-9]{5})")
+
 CS_RE = re.compile(r"""CONVECTIVE\sSIGMET\s(?P<label>[0-9A-Z]+)\s
 VALID\sUNTIL\s(?P<hour>[0-2][0-9])(?P<minute>[0-5][0-9])Z\s
 (?P<states>[A-Z ]+)\s
-(?P<from>FROM)\s?(?P<locs>[0-9A-Z \-]+?)\s
+(?P<from>FROM)?\s?(?P<locs>[0-9A-Z \-]+?)\s
 (?P<dmshg>DMSHG|DVLPG|INTSF)?\s?(?P<geotype>AREA|LINE|ISOL)?\s?
 (?P<cutype>EMBD|SEV|SEV\sEMBD|EMBD\sSEV)?\s?TS\s(?P<width>[0-9]+\sNM\sWIDE)?(?P<diameter>D[0-9]+)?
-""", re.VERBOSE )
+""", re.VERBOSE)
 
 FROM_RE = re.compile(r"""
 (?P<offset>[0-9]+)?(?P<drct>N|NE|NNE|ENE|E|ESE|SE|SSE|S|SSW|SW|WSW|W|WNW|NW|NNW)?\s?(?P<loc>[A-Z0-9]{3})
@@ -48,8 +51,9 @@ dirs = {'NNE': 22.5, 'ENE': 67.5, 'NE':  45.0, 'E': 90.0, 'ESE': 112.5,
 
 KM_SM = 1.609347
 
+
 class SIGMET(object):
-    
+
     def __init__(self):
         """ Constructor """
         self.sts = None
@@ -60,9 +64,11 @@ class SIGMET(object):
         self.centers = []
         self.raw = None
 
+
 class SIGMETException(Exception):
     ''' Exception '''
     pass
+
 
 def figure_expire(ptime, hour, minute):
     """
@@ -71,27 +77,34 @@ def figure_expire(ptime, hour, minute):
     expire = ptime
     if hour < ptime.hour:
         expire += datetime.timedelta(days=1)
-    return expire.replace(hour=hour,minute=minute)
+    return expire.replace(hour=hour, minute=minute)
+
 
 def go2lonlat(lon0, lat0, direction, displacement):
     # http://stackoverflow.com/questions/7222382
-    R = 6378.1 #Radius of the Earth
-    brng = math.radians(dirs[direction]) #Bearing is 90 degrees converted to radians.
-    d = displacement / KM_SM #Distance in km
+    # Radius of the Earth
+    R = 6378.1
+    # Bearing is 90 degrees converted to radians.
+    brng = math.radians(dirs[direction])
+    # Distance in km
+    d = displacement / KM_SM
 
-    lat1 = math.radians(lat0) #Current lat point converted to radians
-    lon1 = math.radians(lon0) #Current long point converted to radians
+    # Current lat point converted to radians
+    lat1 = math.radians(lat0)
+    # Current long point converted to radians
+    lon1 = math.radians(lon0)
 
-    lat2 = math.asin( math.sin(lat1)*math.cos(d/R) +
-                       math.cos(lat1)*math.sin(d/R)*math.cos(brng))
+    lat2 = math.asin(math.sin(lat1)*math.cos(d/R) +
+                     math.cos(lat1)*math.sin(d/R)*math.cos(brng))
 
     lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),
-             math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
+                             math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
 
     lat2 = math.degrees(lat2)
     lon2 = math.degrees(lon2)
 
     return lon2, lat2
+
 
 def locs2lonslats(loc_provider, locstr, geotype, widthstr, diameterstr):
     """
@@ -99,28 +112,19 @@ def locs2lonslats(loc_provider, locstr, geotype, widthstr, diameterstr):
     """
     lats = []
     lons = []
-    #if geotype == 'LINE':
-    #    width = float(widthstr.replace(" NM WIDE", ""))
-        # Approximation
-    #    widthdeg = width / 110.
-
-    #print "locstr is:%s geotype is:%s" % (locstr, geotype)
     for l in locstr.split('-'):
         s = FROM_RE.search(l)
         if s:
             d = s.groupdict()
             if d['offset'] is not None:
-                (lon1, lat1) = go2lonlat(loc_provider[d['loc']]['lon'], 
-                                         loc_provider[d['loc']]['lat'], 
-                                        d['drct'], float(d['offset']) )
+                (lon1, lat1) = go2lonlat(loc_provider[d['loc']]['lon'],
+                                         loc_provider[d['loc']]['lat'],
+                                         d['drct'], float(d['offset']))
             else:
-                (lon1, lat1) = (loc_provider[d['loc']]['lon'], 
+                (lon1, lat1) = (loc_provider[d['loc']]['lon'],
                                 loc_provider[d['loc']]['lat'])
-            lats.append( lat1 )
-            lons.append( lon1 )
-            #print '%s [%s,%s] -> %s,%s' % (l, loc_provider[d['loc']]['lon'],
-            #                               loc_provider[d['loc']]['lat'],
-            #                               lon1, lat1)
+            lats.append(lat1)
+            lons.append(lon1)
     if geotype == 'ISOL' or diameterstr is not None:
         lats2 = []
         lons2 = []
@@ -128,20 +132,20 @@ def locs2lonslats(loc_provider, locstr, geotype, widthstr, diameterstr):
         # Approximation
         diameterdeg = diameter / 110.
         # UR
-        lons2.append( lons[0] - diameterdeg )
-        lats2.append( lats[0] + diameterdeg )
+        lons2.append(lons[0] - diameterdeg)
+        lats2.append(lats[0] + diameterdeg)
         # UL
-        lons2.append( lons[0] + diameterdeg )
-        lats2.append( lats[0] + diameterdeg )
+        lons2.append(lons[0] + diameterdeg)
+        lats2.append(lats[0] + diameterdeg)
         # LL
-        lons2.append( lons[0] + diameterdeg )
-        lats2.append( lats[0] - diameterdeg )
+        lons2.append(lons[0] + diameterdeg)
+        lats2.append(lats[0] - diameterdeg)
         # LR
-        lons2.append( lons[0] - diameterdeg )
-        lats2.append( lats[0] - diameterdeg )
+        lons2.append(lons[0] - diameterdeg)
+        lats2.append(lats[0] - diameterdeg)
         lons = lons2
         lats = lats2
-        
+
     if geotype == 'LINE':
         lats2 = []
         lons2 = []
@@ -155,12 +159,12 @@ def locs2lonslats(loc_provider, locstr, geotype, widthstr, diameterstr):
             runx = 0.1 * math.cos(angle)
             runy = 0.1 * math.sin(angle)
             # UR
-            lons2.append( lons[i] - runy )
-            lats2.append( lats[i] + runx )
+            lons2.append(lons[i] - runy)
+            lats2.append(lats[i] + runx)
             # UL
-            lons2.append( lons[i+1] - runy )
-            lats2.append( lats[i+1] + runx )
-            
+            lons2.append(lons[i+1] - runy)
+            lats2.append(lats[i+1] + runx)
+
         for i in range(0, len(lats)-1):
             deltax = lons[i+1] - lons[i]
             deltay = lats[i+1] - lats[i]
@@ -170,16 +174,17 @@ def locs2lonslats(loc_provider, locstr, geotype, widthstr, diameterstr):
             runx = 0.1 * math.cos(angle)
             runy = 0.1 * math.sin(angle)
             # LL
-            lons2.append( lons[i+1] + runy )
-            lats2.append( lats[i+1] - runx )
+            lons2.append(lons[i+1] + runy)
+            lats2.append(lats[i+1] - runx)
             # LR
-            lons2.append( lons[i] + runy )
-            lats2.append( lats[i] - runx )
+            lons2.append(lons[i] + runy)
+            lats2.append(lats[i] - runx)
 
         lons = lons2
         lats = lats2
 
     return lons, lats
+
 
 def compute_esol(pts, distance):
     """ Figure out the box points given the two points and the distance """
@@ -190,32 +195,33 @@ def compute_esol(pts, distance):
     linedistance = (deltax**2 + deltay**2)**0.5
     deltax = deltax / linedistance
     deltay = deltay / linedistance
-    N = distance / 111.0 # approx
-    newpts.append( [ pts[0][0] - N*deltay, pts[0][1] + N*deltax] )
-    newpts.append( [ pts[1][0] - N*deltay, pts[1][1] + N*deltax] )
-    newpts.append( [ pts[1][0] + N*deltay, pts[1][1] - N*deltax] )
-    newpts.append( [ pts[0][0] + N*deltay, pts[0][1] - N*deltax] )
-    newpts.append( [ newpts[0][0], newpts[0][1] ])
+    N = distance / 111.0  # approx
+    newpts.append([pts[0][0] - N*deltay, pts[0][1] + N*deltax])
+    newpts.append([pts[1][0] - N*deltay, pts[1][1] + N*deltax])
+    newpts.append([pts[1][0] + N*deltay, pts[1][1] - N*deltax])
+    newpts.append([pts[0][0] + N*deltay, pts[0][1] - N*deltax])
+    newpts.append([newpts[0][0], newpts[0][1]])
 
     return newpts
 
-class SIGMETProduct( TextProduct ):
-    '''
+
+class SIGMETProduct(TextProduct):
+    """
     Represents a Storm Prediction Center Mesoscale Convective Discussion
-    '''
-    
-    def __init__(self, text, utcnow=None, ugc_provider=None, 
+    """
+
+    def __init__(self, text, utcnow=None, ugc_provider=None,
                  nwsli_provider=None):
         ''' constructor '''
         TextProduct.__init__(self, text, utcnow, ugc_provider, nwsli_provider)
         self.sigmets = []
-        if self.afos in ['SIGC','SIGW','SIGE']:
+        if self.afos in ['SIGC', 'SIGW', 'SIGE']:
             self.process_SIGC()
         elif self.afos[:2] == 'WS':
             self.process_WS()
         else:
             self.process_ocean()
-            
+
     def sql(self, txn):
         """ Do SQL related stuff that is required """
         txn.execute("DELETE from sigmets_current where expire < now()")
@@ -225,32 +231,33 @@ class SIGMETProduct( TextProduct ):
                 args = (sigmet.label, sigmet.ets)
                 txn.execute(sql, args)
                 sqlwkt = "SRID=4326;%s" % (sigmet.geom.wkt,)
-                sql = """INSERT into """+table+"""(sigmet_type, label, issue, 
+                sql = """
+                    INSERT into """ + table + """(sigmet_type, label, issue,
                     expire, raw, geom) VALUES ('C',%s, %s, %s, %s,
-                   %s)""" 
-                args = (sigmet.label, self.valid, sigmet.ets, sigmet.raw, sqlwkt)
+                   %s)"""
+                args = (sigmet.label, self.valid, sigmet.ets, sigmet.raw,
+                        sqlwkt)
                 txn.execute(sql, args)
             # Compute who is impacted by this SIGMET
             txn.execute("""
             SELECT distinct id from cwsu WHERE
-            st_overlaps(geomFromEWKT('SRID=4326;%s'), geom) or 
-            st_contains(geomFromEWKT('SRID=4326;%s'), geom) """ % (
-                                                        sigmet.geom, 
-                                                        sigmet.geom))
+            st_overlaps(geomFromEWKT('SRID=4326;%s'), geom) or
+            st_contains(geomFromEWKT('SRID=4326;%s'), geom)
+            """ % (sigmet.geom, sigmet.geom))
             for row in txn:
-                sigmet.centers.append( row['id'] )
-                
+                sigmet.centers.append(row['id'])
+
     def compute_time(self, ddhhmi):
         """ Convert this string into a proper date time """
         day = int(ddhhmi[:2])
         hour = int(ddhhmi[2:4])
         minute = int(ddhhmi[4:6])
         ts = self.valid
-        if self.valid.day > 25 and day < 5: # next month
+        if self.valid.day > 25 and day < 5:  # next month
             ts += datetime.timedelta(days=15)
-        
+
         return ts.replace(day=day, hour=hour, minute=minute)
-        
+
     def process_ocean(self):
         """ Process oceananic """
         meat = self.unixtext.replace("\n", " ")
@@ -276,7 +283,7 @@ class SIGMETProduct( TextProduct ):
             lon = float(pair[1][1:]) / 100.0
             if pair[1][0] == 'W':
                 lon = 0 - lon
-            pts.append((lon,lat))
+            pts.append((lon, lat))
         m = LINE_RE.search(meat)
         if m is not None:
             d = m.groupdict()
@@ -288,9 +295,9 @@ class SIGMETProduct( TextProduct ):
             s.geom = Point(pts[0]).buffer(float(d['distance'])*KM_SM / 100.)
         else:
             s.geom = Polygon(pts)
-        
+
         s.raw = self.unixtext
-        self.sigmets.append( s )
+        self.sigmets.append(s)
 
     def process_SIGC(self):
         """ Process this type of SIGMET """
@@ -298,69 +305,68 @@ class SIGMETProduct( TextProduct ):
             s = CS_RE.search(section.replace("\n", ' '))
             if s is None:
                 continue
-                #raise SIGMETException("Failed to parse CS_RE: %s" % (section,))
             data = s.groupdict()
             sig = SIGMET()
             sig.label = data['label']
-            sig.areatext = data['states']
-            sig.ets = figure_expire(self.valid, int(data['hour']), 
-                               int(data['minute']))
+            sig.areatext = data['states'].replace(" FROM", "")
+            sig.ets = figure_expire(self.valid, int(data['hour']),
+                                    int(data['minute']))
             lons, lats = locs2lonslats(self.nwsli_provider,
-                                       data['locs'], data['geotype'], 
+                                       data['locs'], data['geotype'],
                                        data['width'], data['diameter'])
-            
+
             if len(lons) == 2:
                 continue
             pts = []
-            for lon,lat in zip(lons,lats):
+            for lon, lat in zip(lons, lats):
                 pts.append((lon, lat))
             if lats[0] != lats[-1] or lons[0] != lons[-1]:
                 pts.append((lons[0], lats[0]))
             sig.geom = Polygon(pts)
             sig.raw = section
-      
+
             self.sigmets.append(sig)
-      
+
     def process_WS(self):
         """ Process this type of SIGMET """
         pass
-    
+
     def get_jabbers(self, uri, uri2):
         """ Return the Jabber for this sigmet """
         j = []
         for sig in self.sigmets:
-            area = " for "+ sig.areatext if sig.areatext != "" else ""
-            txt = "%s issues SIGMET %s%s till %s UTC" % (self.source, sig.label,
-                                                  area,
-                                                  sig.ets.strftime("%H%M"))
-            html = "<p>%s issues SIGMET %s%s till %s UTC</p>" % (self.source, 
-                                                          sig.label,
-                                                          area,
-                                                sig.ets.strftime("%H%M"))
+            area = " for " + sig.areatext if sig.areatext != "" else ""
+            txt = ("%s issues SIGMET %s%s till %s UTC"
+                   ) % (self.source, sig.label, area,
+                        sig.ets.strftime("%H%M"))
+            html = ("<p>%s issues SIGMET %s%s till %s UTC</p>"
+                    ) % (self.source, sig.label, area,
+                         sig.ets.strftime("%H%M"))
             channels = ["SIGMET.%s" % (i,) for i in sig.centers]
             channels.append("SIGMET.%s" % (self.source[1:],))
             xtra = {
                     'channels': ",".join(channels),
                     'twitter': txt
                     }
-            
-            j.append( [txt,html,xtra] )
+
+            j.append([txt, html, xtra])
         return j
-    
+
     def draw(self):
         ''' For debugging, draw the polygons!'''
         from descartes.patch import PolygonPatch
         from pyiem.plot import MapPlot
         for sig in self.sigmets:
             m = MapPlot(sector='conus')
-            x,y = m.map(sig.geom.exterior.xy[0], sig.geom.exterior.xy[1])
-            patch = PolygonPatch(Polygon(zip(x,y)), fc='r', label='Outlook')
+            x, y = m.map(sig.geom.exterior.xy[0], sig.geom.exterior.xy[1])
+            patch = PolygonPatch(Polygon(zip(x, y)), fc='r', label='Outlook')
             m.ax.add_patch(patch)
             fn = '/tmp/%s.png' % (sig.label,)
             print ':: creating plot %s' % (fn,)
-            m.postprocess(filename=fn )
+            m.postprocess(filename=fn)
             m.close()
+
 
 def parser(text, utcnow=None, ugc_provider=None, nwsli_provider=None):
     ''' Helper function '''
-    return SIGMETProduct( text, utcnow, ugc_provider, nwsli_provider )
+    return SIGMETProduct(text, utcnow, ugc_provider, nwsli_provider)
--- pyiem/nws/tests/test_sigmet.py
+++ pyiem/nws/tests/test_sigmet.py
@@ -5,22 +5,25 @@ import pytz
 
 from pyiem.nws.products.sigmet import parser, compute_esol
 
+
 def get_file(name):
     ''' Helper function to get the text file contents '''
     basedir = os.path.dirname(__file__)
     fn = "%s/../../../data/product_examples/%s" % (basedir, name)
     return open(fn).read()
 
+
 def utc(year, month, day, hour=0, minute=0):
     """UTC Timestamp generator"""
     return datetime.datetime(year, month, day, hour, minute).replace(
                         tzinfo=pytz.timezone("UTC"))
 
-class TestObservation(unittest.TestCase):
-   
+
+class TestSIGMET(unittest.TestCase):
+
     def test_compute_esol(self):
         """ Test our algo on either side of a line """
-        pts = [ [0,0], [5,0] ]
+        pts = [[0, 0], [5, 0]]
         pts = compute_esol(pts, 111)
         print pts
         self.assertAlmostEqual(pts[0][0], 0.00, 2)
@@ -33,73 +36,105 @@ class TestObservation(unittest.TestCase):
         self.assertAlmostEqual(pts[3][1], -1.00, 2)
         self.assertAlmostEqual(pts[4][0], 0.00, 2)
         self.assertAlmostEqual(pts[4][1], 1.00, 2)
-    
+
+    def test_150915_line(self):
+        """ See about parsing a SIGMET LINE """
+        utcnow = utc(2015, 9, 15, 2, 55)
+        ugc_provider = {}
+        nwsli_provider = {
+            "MSP": dict(lon=-83.39, lat=44.45),
+            "MCW": dict(lon=-85.50, lat=42.79),
+        }
+        tp = parser(get_file('SIGC_line.txt'), utcnow, ugc_provider,
+                    nwsli_provider)
+        self.assertAlmostEquals(tp.sigmets[0].geom.area, 0.47, 2)
+
+    def test_150915_isol(self):
+        """ See about parsing a SIGMET ISOL """
+        utcnow = utc(2015, 9, 12, 23, 55)
+        ugc_provider = {}
+        nwsli_provider = {
+            "FTI": dict(lon=-83.39, lat=44.45),
+            "CME": dict(lon=-85.50, lat=42.79),
+        }
+        tp = parser(get_file('SIGC_ISOL.txt'), utcnow, ugc_provider,
+                    nwsli_provider)
+        self.assertAlmostEquals(tp.sigmets[0].geom.area, 0.30, 2)
+        self.assertAlmostEquals(tp.sigmets[1].geom.area, 0.30, 2)
+
+    def test_150915_nospace(self):
+        """ See about parsing a SIGMET that has no spaces """
+        utcnow = utc(2015, 9, 15, 15, 41)
+        tp = parser(get_file('SIGAX.txt'), utcnow)
+        self.assertAlmostEquals(tp.sigmets[0].geom.area, 23.47, 2)
+
     def test_140907_circle(self):
         """ See about parsing a SIGMET that is circle? """
         utcnow = utc(2014, 9, 6, 22, 15)
-        tp = parser( get_file('SIGP0H.txt'), utcnow)
+        tp = parser(get_file('SIGP0H.txt'), utcnow)
         self.assertAlmostEquals(tp.sigmets[0].geom.area, 11.70, 2)
 
     def test_140813_line(self):
         """ See about parsing a SIGMET that is a either side of line """
         utcnow = utc(2014, 8, 12, 13, 15)
-        tp = parser( get_file('SIGP0A_line.txt'), utcnow)
+        tp = parser(get_file('SIGP0A_line.txt'), utcnow)
         self.assertAlmostEquals(tp.sigmets[0].geom.area, 4.32, 2)
 
     def test_140815_cancel(self):
         """ See about parsing a SIGMET that is a either side of line """
         utcnow = utc(2014, 8, 15, 23, 41)
-        tp = parser( get_file('SIG_cancel.txt'), utcnow)
+        tp = parser(get_file('SIG_cancel.txt'), utcnow)
         self.assertAlmostEquals(len(tp.sigmets), 0)
-   
+
     def test_sigaoa(self):
         """ SIGAOA """
         utcnow = utc(2014, 8, 11, 19, 15)
-        tp = parser( get_file('SIGA0A.txt'), utcnow)
+        tp = parser(get_file('SIGA0A.txt'), utcnow)
         self.assertAlmostEquals(tp.sigmets[0].geom.area, 24.35, 2)
-   
+
     def test_sigaob(self):
         """ See about parsing 50E properly """
         utcnow = utc(2014, 8, 11, 19, 15)
-        tp = parser( get_file('SIGA0B.txt'), utcnow)
-        self.assertEquals( len(tp.sigmets), 0)
-    
+        tp = parser(get_file('SIGA0B.txt'), utcnow)
+        self.assertEquals(len(tp.sigmets), 0)
+
     def test_50e(self):
         """ See about parsing 50E properly """
         utcnow = utc(2014, 8, 11, 18, 55)
         ugc_provider = {}
-        nwsli_provider= {
+        nwsli_provider = {
             "ASP": dict(lon=-83.39, lat=44.45),
             "ECK": dict(lon=-82.72, lat=43.26),
             "GRR": dict(lon=-85.50, lat=42.79),
         }
-            
-        tp = parser( get_file('SIGE3.txt'), utcnow, ugc_provider, nwsli_provider )
-        #tp.draw()
+
+        tp = parser(get_file('SIGE3.txt'), utcnow, ugc_provider,
+                    nwsli_provider)
+        # tp.draw()
         self.assertAlmostEquals(tp.sigmets[0].geom.area, 2.15, 2)
-    
+
     def test_sigc(self):
         """ See about parsing SIGC """
         utcnow = utc(2014, 8, 11, 16, 55)
         ugc_provider = {}
-        nwsli_provider= {}
+        nwsli_provider = {}
         for sid in ("MSL,SJI,MLU,LIT,BTR,LEV,LCH,IAH,YQT,SAW,SAT,DYC,AXC,"
-                    +"ODI,DEN,TBE,ADM,JCT,INK,ELP").split(","):
+                    "ODI,DEN,TBE,ADM,JCT,INK,ELP").split(","):
             nwsli_provider[sid] = dict(lon=-99, lat=45)
-            
-        tp = parser( get_file('SIGC.txt'), utcnow, ugc_provider, nwsli_provider )
-        #tp.draw()
+
+        tp = parser(get_file('SIGC.txt'), utcnow, ugc_provider, nwsli_provider)
+        # tp.draw()
         j = tp.get_jabbers('http://localhost', 'http://localhost')
         self.assertEquals(tp.sigmets[0].ets, utc(2014, 8, 11, 18, 55))
         self.assertEquals(j[0][0], ('KKCI issues SIGMET 62C for AL MS LA AR '
-                                    +'till 1855 UTC'))
+                                    'till 1855 UTC'))
         self.assertEquals(j[1][0], ('KKCI issues SIGMET 63C for LA TX AND MS '
-                                    +'LA TX CSTL WTRS till 1855 UTC'))
-    
+                                    'LA TX CSTL WTRS till 1855 UTC'))
+
     def test_sigpat(self):
         """ Make sure we don't have another failure with geom parsing """
         utcnow = utc(2014, 8, 11, 12, 34)
-        tp = parser( get_file('SIGPAT.txt'), utcnow )
+        tp = parser(get_file('SIGPAT.txt'), utcnow)
         j = tp.get_jabbers('http://localhost', 'http://localhost')
         self.assertAlmostEquals(tp.sigmets[0].geom.area, 33.71, 2)
         self.assertEquals(tp.sigmets[0].sts, utc(2014, 8, 11, 12, 35))
