3ab03fdad0a1c20c8feb60e1ff6db1d83ed41460
akrherz <akrherz@iastate.edu>
2015-09-15 14:37:50 -0500

    fix pep8 and landscape issues with nws.products.cli

--- pyiem/nws/products/cli.py
+++ pyiem/nws/products/cli.py
@@ -1,53 +1,57 @@
-"""Parser and object storage of information within NWS CLI Product. 
+"""Parser and object storage of information within NWS CLI Product.
 """
 import re
 import datetime
 
 from pyiem.nws.product import TextProduct
 
-HEADLINE_RE = re.compile(r"\.\.\.THE ([A-Z_\.\-\(\)\/\,\s]+) CLIMATE SUMMARY FOR\s+([A-Z]+\s[0-9]+\s+[0-9]{4})( CORRECTION)?\.\.\.")
+HEADLINE_RE = re.compile((r"\.\.\.THE ([A-Z_\.\-\(\)\/\,\s]+) "
+                          "CLIMATE SUMMARY FOR\s+"
+                          "([A-Z]+\s[0-9]+\s+[0-9]{4})( CORRECTION)?\.\.\."))
 
 REGIMES = [
-"WEATHER ITEM   OBSERVED TIME   RECORD YEAR NORMAL DEPARTURE LAST",
-"WEATHER ITEM   OBSERVED TIME   NORMAL DEPARTURE LAST",
-"WEATHER ITEM   OBSERVED TIME    RECORD YEAR NORMAL DEPARTURE LAST",
-"WEATHER ITEM   OBSERVED RECORD YEAR NORMAL DEPARTURE LAST",
-"WEATHER ITEM   OBSERVED TIME   RECORD YEAR",
-"WEATHER ITEM   OBSERVED TIME   RECORD YEAR NORMAL DEPARTURE",
-"WEATHER ITEM   OBSERVED RECORD YEAR NORMAL DEPARTURE",
-"WEATHER ITEM   OBSERVED",
-"WEATHER ITEM   OBSERVED RECORD YEAR NORMAL",
-"WEATHER ITEM   OBSERVED TIME   RECORD YEAR NORMAL  LAST",
-"WEATHER ITEM   OBSERVED TIME       LAST",
-"WEATHER ITEM   OBSERVED NORMAL DEPARTURE LAST",
-"WEATHER ITEM   OBSERVED TIME   NORMAL  LAST",
-"WEATHER ITEM   OBSERVED TIME   RECORD YEAR     LAST",
-"WEATHER ITEM   OBSERVED TIME",
-"WEATHER ITEM   OBSERVED TIME   NORMAL DEPARTURE",
+    "WEATHER ITEM   OBSERVED TIME   RECORD YEAR NORMAL DEPARTURE LAST",
+    "WEATHER ITEM   OBSERVED TIME   NORMAL DEPARTURE LAST",
+    "WEATHER ITEM   OBSERVED TIME    RECORD YEAR NORMAL DEPARTURE LAST",
+    "WEATHER ITEM   OBSERVED RECORD YEAR NORMAL DEPARTURE LAST",
+    "WEATHER ITEM   OBSERVED TIME   RECORD YEAR",
+    "WEATHER ITEM   OBSERVED TIME   RECORD YEAR NORMAL DEPARTURE",
+    "WEATHER ITEM   OBSERVED RECORD YEAR NORMAL DEPARTURE",
+    "WEATHER ITEM   OBSERVED",
+    "WEATHER ITEM   OBSERVED RECORD YEAR NORMAL",
+    "WEATHER ITEM   OBSERVED TIME   RECORD YEAR NORMAL  LAST",
+    "WEATHER ITEM   OBSERVED TIME       LAST",
+    "WEATHER ITEM   OBSERVED NORMAL DEPARTURE LAST",
+    "WEATHER ITEM   OBSERVED TIME   NORMAL  LAST",
+    "WEATHER ITEM   OBSERVED TIME   RECORD YEAR     LAST",
+    "WEATHER ITEM   OBSERVED TIME",
+    "WEATHER ITEM   OBSERVED TIME   NORMAL DEPARTURE",
 ]
-# label, value, time, record, year, normal, departure, last
+#   label, value, time, record, year, normal, departure, last
 COLS = [
-[  16,     23,   30,   37,     42,   49,    56,         65],
-[  16,     23,   30,   None,   None, 37,    44,         53],
-[  16,     22,   31,   37,     43,   50,    58,         65],
-[  16,     23,  None,  30,     35,   42,    49,         58],
-[  16,     23,   25,   37,     42,   None,  None,      None],
-[  16,     23,   30,   37,     42,   49,    56,        None],
-[  16,     23,  None,  30,     35,   42,    49,        None],
-[  16,     23,  None,  None,   None, None,  None,      None],
-[  16,     23,  None,  30,     37,   None,  None,      None],
-[  16,     23,   30,   37,     42,   49,    None,       57],
-[  16,     23,   30,   None,   None,  None,    None,       39],
-[  16,     23,   None,   None,   None,  30,    37,       46],
-[  16,     23,   30,   None,   None,  37,    None,       45],
-[  16,     23,   30,   37,   42,  None,    None,       51],
-[  16,     23,   30,   None,   None,  None,    None,       None],
-[  16,     23,   30,   None,   None,  37,    44,       None],
-]
+    [16,     23,   30,   37,     42,   49,     56,        65],
+    [16,     23,   30,   None,   None, 37,     44,        53],
+    [16,     22,   31,   37,     43,   50,     58,        65],
+    [16,     23,  None,  30,     35,   42,     49,        58],
+    [16,     23,   25,   37,     42,   None,   None,      None],
+    [16,     23,   30,   37,     42,   49,     56,        None],
+    [16,     23,  None,  30,     35,   42,     49,        None],
+    [16,     23,  None,  None,   None, None,   None,      None],
+    [16,     23,  None,  30,     37,   None,   None,      None],
+    [16,     23,   30,   37,     42,   49,     None,      57],
+    [16,     23,   30,   None,   None, None,   None,      39],
+    [16,     23,   None, None,   None, 30,     37,        46],
+    [16,     23,   30,   None,   None, 37,     None,      45],
+    [16,     23,   30,   37,     42,   None,   None,      51],
+    [16,     23,   30,   None,   None, None,   None,      None],
+    [16,     23,   30,   None,   None, 37,     44,        None],
+    ]
+
 
 class CLIException(Exception):
     """ Exception """
     pass
+
 
 def trace(val):
     """ This value could be T or M, account for it! """
@@ -57,11 +61,13 @@ def trace(val):
         return 0.0001
     return float(val)
 
+
 def trace_r(val):
     """ Convert our value back into meaningful string """
     if val == 0.0001:
         return 'Trace'
     return val
+
 
 def get_number(s):
     """ Convert a string into a number, preferable a float! """
@@ -83,6 +89,7 @@ def get_number(s):
     print 'get_number() failed for |%s|' % (s,)
     return None
 
+
 def convert_key(s):
     """ Convert a key value to something we store """
     if s == 'YESTERDAY':
@@ -96,6 +103,7 @@ def convert_key(s):
     print 'convert_key() failed for |%s|' % (s,)
     return 'fail'
 
+
 def make_tokens(regime, line):
     """ Turn a line into tokens based on a regime """
     mycols = COLS[regime]
@@ -105,15 +113,17 @@ def make_tokens(regime, line):
         if e is None:
             tokens.append(None)
             continue
-        tokens.append(line[pos:e].strip() if line[pos:e].strip() != "" else None)
+        tokens.append(
+            line[pos:e].strip() if line[pos:e].strip() != "" else None)
         pos = e
     for i, token in enumerate(tokens):
         if token is not None and token.startswith("R "):
             tokens[i] = token.replace("R ", "")
     return tokens
 
+
 def parse_snowfall(regime, lines, data):
-    """ Parse the snowfall data 
+    """ Parse the snowfall data
     """
     for linenum, line in enumerate(lines):
         # skipme
@@ -128,17 +138,18 @@ def parse_snowfall(regime, lines, data):
         data['snow_%s_record' % (key,)] = get_number(tokens[3])
         yeartest = get_number(tokens[4])
         if yeartest is not None:
-            data['snow_%s_record_years' % (key,)] = [yeartest,]
+            data['snow_%s_record_years' % (key,)] = [yeartest, ]
         data['snow_%s_normal' % (key,)] = get_number(tokens[5])
         data['snow_%s_departure' % (key,)] = get_number(tokens[6])
         data['snow_%s_last' % (key,)] = get_number(tokens[7])
         if (key == 'today' and yeartest is not None and
-            data['snow_%s_record_years' % (key,)][0] is not None):
-                while ((linenum+1)<len(lines) and 
-                       len(lines[linenum+1].strip()) == 4):
-                    data['snow_today_record_years'].append(
-                                                    int(lines[linenum+1]))
-                    linenum += 1
+                data['snow_%s_record_years' % (key, )][0] is not None):
+            while ((linenum+1) < len(lines) and
+                   len(lines[linenum+1].strip()) == 4):
+                data['snow_today_record_years'].append(
+                                                int(lines[linenum+1]))
+                linenum += 1
+
 
 def parse_precipitation(regime, lines, data):
     """ Parse the precipitation data """
@@ -147,28 +158,30 @@ def parse_precipitation(regime, lines, data):
             continue
         tokens = make_tokens(regime, line)
         key = convert_key(tokens[0])
-        
+
         data['precip_%s' % (key,)] = get_number(tokens[1])
         data['precip_%s_record' % (key,)] = get_number(tokens[3])
         yeartest = get_number(tokens[4])
         if yeartest is not None:
-            data['precip_%s_record_years' % (key,)] = [yeartest,]
+            data['precip_%s_record_years' % (key,)] = [yeartest, ]
         data['precip_%s_normal' % (key,)] = get_number(tokens[5])
         data['precip_%s_departure' % (key,)] = get_number(tokens[6])
         data['precip_%s_last' % (key,)] = get_number(tokens[7])
         if (key == 'today' and yeartest is not None and
-            data['precip_%s_record_years' % (key,)][0] is not None):
-                while ((linenum+1)<len(lines) and 
-                       len(lines[linenum+1].strip()) == 4):
-                    data['precip_today_record_years'].append(
-                                                    int(lines[linenum+1]))
-                    linenum += 1
+                data['precip_%s_record_years' % (key,)][0] is not None):
+            while ((linenum+1) < len(lines) and
+                    len(lines[linenum+1].strip()) == 4):
+                data['precip_today_record_years'].append(
+                    int(lines[linenum+1]))
+                linenum += 1
+
 
 def no99(val):
     """ Giveme int val of null! """
     if val == '-99':
         return None
     return int(val)
+
 
 def parse_temperature(regime, lines, data):
     """ Here we parse a temperature section
@@ -190,28 +203,30 @@ def parse_temperature(regime, lines, data):
         if tokens[4] is not None:
             n = get_number(tokens[4])
             if n is not None:
-                data['temperature_%s_record_years' % (key,)] = [n,]
-        if tokens[5] is not None:    
+                data['temperature_%s_record_years' % (key,)] = [n, ]
+        if tokens[5] is not None:
             data['temperature_%s_normal' % (key,)] = get_number(tokens[5])
             # Check next line(s) for more years
-            while ((linenum+1)<len(lines) and 
+            while ((linenum+1) < len(lines) and
                    len(lines[linenum+1].strip()) == 4):
                 data['temperature_%s_record_years' % (key,)].append(
                                                 int(lines[linenum+1]))
                 linenum += 1
+
 
 class CLIProduct(TextProduct):
     """
     Represents a CLI Daily Climate Report Product
     """
 
-    def __init__(self, text):
+    def __init__(self, text, utcnow=None, ugc_provider=None,
+                 nwsli_provider=None):
         """ constructor """
-        TextProduct.__init__(self, text)
+        TextProduct.__init__(self, text, utcnow, ugc_provider, nwsli_provider)
         # Hold our parsing results as an array of dicts
         self.data = []
         self.regime = None
-        # Sometimes, we get products that are not really in CLI format but 
+        # Sometimes, we get products that are not really in CLI format but
         # are RER (record event reports) with a CLI AWIPS ID
         if self.wmo[:2] != 'CD':
             print 'Product %s skipped due to wrong header' % (
@@ -237,45 +252,45 @@ class CLIProduct(TextProduct):
             raise CLIException("Found %s 'WEATHER ITEM' in text" % (
                                                                 len(tokens),))
         diction = tokens[0].strip()
-        if not diction in REGIMES:
+        if diction not in REGIMES:
             raise CLIException(("Unknown diction found in 'WEATHER ITEM'\n"
-                                +"|%s|") % (diction,))
-        
+                                "|%s|") % (diction,))
+
         self.regime = REGIMES.index(diction)
-        
+
     def get_jabbers(self, uri, _=None):
         """ Override the jabber message formatter """
         url = "%s?pid=%s" % (uri, self.get_product_id())
         res = []
         for data in self.data:
-            mess = "%s %s Climate Report: High: %s Low: %s Precip: %s Snow: %s %s" % (
-                        data['cli_station'], 
-                        data['cli_valid'].strftime("%b %-d"),
-                        data['data'].get('temperature_maximum', 'M'),
-                        data['data'].get('temperature_minimum', 'M'),
-                        trace_r(data['data'].get('precip_today', 'M')),
-                        trace_r(data['data'].get('snow_today', 'M')), url
-                        )
+            mess = ("%s %s Climate Report: High: %s Low: %s "
+                    "Precip: %s Snow: %s %s"
+                    ) % (data['cli_station'],
+                         data['cli_valid'].strftime("%b %-d"),
+                         data['data'].get('temperature_maximum', 'M'),
+                         data['data'].get('temperature_minimum', 'M'),
+                         trace_r(data['data'].get('precip_today', 'M')),
+                         trace_r(data['data'].get('snow_today', 'M')), url)
             htmlmess = ("%s <a href=\"%s\">%s Climate Report</a>: High: %s "
-                        +"Low: %s Precip: %s Snow: %s") % (
-                        data['cli_station'], url, data['cli_valid'].strftime("%b %-d"),
-                        data['data'].get('temperature_maximum', 'M'),
-                        data['data'].get('temperature_minimum', 'M'),
-                        trace_r(data['data'].get('precip_today', 'M')),
-                        trace_r(data['data'].get('snow_today', 'M'))
-                        )
-            tweet = "%s %s Climate: Hi: %s Lo: %s Precip: %s Snow: %s %s" % (
-                        data['cli_station'], data['cli_valid'].strftime("%b %-d"),
-                        data['data'].get('temperature_maximum', 'M'),
-                        data['data'].get('temperature_minimum', 'M'),
-                        trace_r(data['data'].get('precip_today', 'M')),
-                        trace_r(data['data'].get('snow_today', 'M')), url
-                        )
-            res.append([mess.replace("0.0001", "Trace"), 
+                        "Low: %s Precip: %s Snow: %s"
+                        ) % (data['cli_station'], url,
+                             data['cli_valid'].strftime("%b %-d"),
+                             data['data'].get('temperature_maximum', 'M'),
+                             data['data'].get('temperature_minimum', 'M'),
+                             trace_r(data['data'].get('precip_today', 'M')),
+                             trace_r(data['data'].get('snow_today', 'M')))
+            tweet = ("%s %s Climate: Hi: %s Lo: %s Precip: %s Snow: %s %s"
+                     ) % (data['cli_station'],
+                          data['cli_valid'].strftime("%b %-d"),
+                          data['data'].get('temperature_maximum', 'M'),
+                          data['data'].get('temperature_minimum', 'M'),
+                          trace_r(data['data'].get('precip_today', 'M')),
+                          trace_r(data['data'].get('snow_today', 'M')), url)
+            res.append([mess.replace("0.0001", "Trace"),
                         htmlmess.replace("0.0001", "Trace"), {
                             'channels': self.afos,
                             'twitter': tweet
-                    }])
+                            }])
         return res
 
     def parse_data(self, section):
@@ -328,4 +343,4 @@ class CLIProduct(TextProduct):
 def parser(text, utcnow=None, ugc_provider=None, nwsli_provider=None):
     """ Provide back CLI objects based on the parsing of this text """
     # Careful here, see if we have two CLIs in one product!
-    return CLIProduct(text)
+    return CLIProduct(text, utcnow, ugc_provider, nwsli_provider)
