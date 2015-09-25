8d725e329b1d0eedf4cc7bdff6cacd4428d0003f
akrherz <akrherz@iastate.edu>
2015-09-25 14:29:43 -0500

    increase coverage of pyiem.network

--- pyiem/network.py
+++ pyiem/network.py
@@ -3,20 +3,22 @@ import psycopg2.extras
 
 class Table(object):
 
-    def __init__(self, network):
+    def __init__(self, network, cursor=None):
         """A class representing a network(s) of IEM metadata
 
         Args:
           network (str or list): A network identifier used by the IEM, this can
             be either a string or a list of strings.
+          cursor (dbcursor,optional): A database cursor to use for the query
         """
         self.sts = {}
         if network is None:
             return
 
-        dbconn = psycopg2.connect(database='mesosite', host='iemdb',
-                                  user='nobody')
-        cursor = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        if cursor is None:
+            dbconn = psycopg2.connect(database='mesosite', host='iemdb',
+                                      user='nobody')
+            cursor = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         if isinstance(network, str):
             network = [network, ]
         for n in network:
@@ -25,8 +27,4 @@ class Table(object):
                 from stations WHERE network = %s ORDER by name ASC
                 """, (n,))
             for row in cursor:
-                self.sts[row['id']] = dict()
-                for key in row.keys():
-                    self.sts[row['id']][key] = row[key]
-        cursor.close()
-        dbconn.close()
+                self.sts[row['id']] = dict(row)
--- pyiem/tests/test_network.py
+++ pyiem/tests/test_network.py
@@ -1,13 +1,30 @@
+import psycopg2.extras
 import unittest
 from pyiem import network
 
 
 class TestNetwork(unittest.TestCase):
 
+    def setUp(self):
+        self.conn = psycopg2.connect(database='mesosite', host='iemdb')
+        self.cursor = self.conn.cursor(
+            cursor_factory=psycopg2.extras.DictCursor)
+        self.cursor.execute("""INSERT into stations(id, name, network)
+        VALUES ('BOGUS', 'BOGUS NAME', 'BOGUS') RETURNING iemid""")
+        self.cursor.execute("""INSERT into stations(id, name, network)
+        VALUES ('BOGUS2', 'BOGUS2 NAME', 'BOGUS')""")
+        self.cursor.execute("""INSERT into stations(id, name, network)
+        VALUES ('BOGUS3', 'BOGUS3 NAME', 'BOGUS2')""")
+
     def test_basic(self):
         ''' basic test of constructor '''
-        nt = network.Table("BOGUS")
-        self.assertEqual(len(nt.sts.keys()), 0)
+        nt = network.Table("BOGUS", cursor=self.cursor)
+        self.assertEqual(len(nt.sts.keys()), 2)
+
+        nt = network.Table(["BOGUS", "BOGUS2"], cursor=self.cursor)
+        self.assertEqual(len(nt.sts.keys()), 3)
+
+        self.assertEqual(nt.sts['BOGUS']['name'], 'BOGUS NAME')
 
         nt = network.Table(["BOGUS", "BOGUS2"])
         self.assertEqual(len(nt.sts.keys()), 0)
