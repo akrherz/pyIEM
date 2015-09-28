dfc8329208ef1434d0cc44fae7f2f41a0ff7624c
akrherz <akrherz@iastate.edu>
2015-09-28 15:00:40 -0500

    pep8 fixes

--- pyiem/nws/products/tests/test_pirep.py
+++ pyiem/nws/products/tests/test_pirep.py
@@ -4,16 +4,19 @@ import pytz
 import unittest
 from pyiem.nws.products.pirep import parser as pirepparser
 
+
 def get_file(name):
     ''' Helper function to get the text file contents '''
     basedir = os.path.dirname(__file__)
     fn = "%s/../../../../data/product_examples/%s" % (basedir, name)
     return open(fn).read()
 
+
 def utc(year, month, day, hour=0, minute=0):
     """UTC Timestamp generator"""
     return datetime.datetime(year, month, day, hour, minute).replace(
                         tzinfo=pytz.timezone("UTC"))
+
 
 class TestProducts(unittest.TestCase):
     """ Tests """
@@ -36,7 +39,7 @@ class TestProducts(unittest.TestCase):
                            nwsli_provider=nwsli_provider)
         self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
         self.assertAlmostEquals(prod.reports[0].latitude, 44.15, 2)
-        
+
     def test_150121_offset(self):
         """ offset.txt and yet another /OV iteration """
         nwsli_provider = {'MRF': {'lat': 44.26, 'lon': -88.52},
@@ -58,7 +61,7 @@ class TestProducts(unittest.TestCase):
         self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
         self.assertAlmostEquals(prod.reports[0].latitude, 44.26, 2)
         self.assertAlmostEquals(prod.reports[1].longitude, -88.52, 2)
-        
+
     def test_150121_fourchar(self):
         """ Another coding edition with four char identifiers """
         nwsli_provider = {'FAR': {'lat': 44, 'lon': -99},
@@ -69,7 +72,7 @@ class TestProducts(unittest.TestCase):
         self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
         self.assertAlmostEquals(prod.reports[0].latitude, 44.10, 2)
         self.assertAlmostEquals(prod.reports[1].latitude, 42.50, 2)
-    
+
     def test_150120_latlonloc(self):
         """ latlonloc.txt Turns out there is a LAT/LON option for OV """
         prod = pirepparser(get_file('PIREPS/latlonloc.txt'))
@@ -83,10 +86,10 @@ class TestProducts(unittest.TestCase):
         self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
 
         nwsli_provider = {'PKTN': {'lat': 44, 'lon': -99}}
-        prod = pirepparser(get_file('PIREPS/PKTN.txt'), nwsli_provider=nwsli_provider)
+        prod = pirepparser(get_file('PIREPS/PKTN.txt'),
+                           nwsli_provider=nwsli_provider)
         self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
 
-    
     def test_150120_OVO(self):
         """ PIREPS/OVO.txt has a location of OV 0 """
         nwsli_provider = {'AVK': {'lat': 44, 'lon': 99}}
@@ -99,7 +102,7 @@ class TestProducts(unittest.TestCase):
         lat = 42.5
         lon = -92.5
         nwsli_provider = {'BIL': {'lat': lat, 'lon': lon}}
-        p = pirepparser("\001\r\r\n000 \r\r\nUBUS01 KMSC 090000\r\r\n", 
+        p = pirepparser("\001\r\r\n000 \r\r\nUBUS01 KMSC 090000\r\r\n",
                         nwsli_provider=nwsli_provider)
         lon2, lat2 = p.compute_loc("BIL", 0, 0)
         self.assertEquals(lon2, lon)
@@ -113,10 +116,9 @@ class TestProducts(unittest.TestCase):
         self.assertEquals(lon2, lon)
         self.assertAlmostEquals(lat2, 43.95, 2)
 
-
     def test_1(self):
         """ PIREP.txt, can we parse it! """
-        utcnow = utc(2015,1,9,0,0)
+        utcnow = utc(2015, 1, 9, 0, 0)
         nwsli_provider = {'BIL': {'lat': 44, 'lon': 99},
                           'LBY': {'lat': 45, 'lon': 100},
                           'PUB': {'lat': 46, 'lon': 101},
@@ -124,6 +126,6 @@ class TestProducts(unittest.TestCase):
         prod = pirepparser(get_file('PIREP.txt'), utcnow=utcnow,
                            nwsli_provider=nwsli_provider)
         self.assertEquals(len(prod.warnings), 0, "\n".join(prod.warnings))
-        
+
         j = prod.get_jabbers()
         self.assertEquals(j[0][2]['channels'], 'UA.None,UA.PIREP')
