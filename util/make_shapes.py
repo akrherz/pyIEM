e73372c8a5428547d1544ae44d57119c8b5aa29c
akrherz <akrherz@iastate.edu>
2016-02-08 10:37:06 -0600

    make a Iowa WFO outline

--- util/make_shapes.py
+++ util/make_shapes.py
@@ -64,9 +64,26 @@ def dump_cwa(fn):
     for row in cursor:
         data[row[0]] = dict(geom=loads(str(row[1])), lon=row[2], lat=row[3],
                             region=row[4])
-        # for polygon in geom:
-        #    data[row[0]].append(np.asarray(polygon.exterior))
+    f = open('../pyiem/data/%s' % (fn, ), 'wb')
+    cPickle.dump(data, f, 2)
+    f.close()
 
+
+def dump_iowawfo(fn):
+    """ A region with the Iowa WFOs"""
+    pgconn = psycopg2.connect(database='postgis', host='iemdb',
+                              user='nobody')
+    cursor = pgconn.cursor()
+
+    cursor.execute(""" SELECT ST_asEWKB(ST_Simplify(ST_Union(the_geom), 0.01))
+        from cwa
+        WHERE wfo in ('DMX', 'ARX', 'DVN', 'OAX', 'FSD')""")
+    row = cursor.fetchone()
+
+    geo = loads(str(row[0]))
+    data = dict()
+    data['iowawfo'] = dict(geom=geo,
+                           lon=geo.centroid.x, lat=geo.centroid.y)
     f = open('../pyiem/data/%s' % (fn, ), 'wb')
     cPickle.dump(data, f, 2)
     f.close()
@@ -100,6 +117,7 @@ def check_file(fn):
 
     print (ets - sts).total_seconds(), len(data.keys()), fn
 
+dump_iowawfo('iowawfo.pickle')
 dump_ugc('C', 'ugcs_county.pickle')
 dump_ugc('Z', 'ugcs_zone.pickle')
 check_file('ugcs_county.pickle')
