b88a840b5afb72a926877c3459eb8974d4e34fdf
akrherz <akrherz@iastate.edu>
2016-07-29 08:39:05 -0500

    fix twistedpg test

--- pyiem/tests/test_twistedpg.py
+++ pyiem/tests/test_twistedpg.py
@@ -1,6 +1,6 @@
 import unittest
 from pyiem import twistedpg
-from psycopg2.extras import RealDictCursor
+from psycopg2.extras import DictCursor
 
 
 class TestTWPG(unittest.TestCase):
@@ -8,4 +8,4 @@ class TestTWPG(unittest.TestCase):
     def test_connect(self):
         conn = twistedpg.connect(database='postgis', host='iemdb')
         cursor = conn.cursor()
-        self.assertTrue(isinstance(cursor, RealDictCursor))
+        self.assertTrue(isinstance(cursor, DictCursor))
