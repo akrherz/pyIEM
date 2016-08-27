8366f2bd970a626b8e6a3fdb7fc73542222cb75b
akrherz <akrherz@iastate.edu>
2016-08-26 22:01:21 -0500

    Introduce HML parsing

---
+++ pyiem/nws/products/hml.py
@@ -0,0 +1,190 @@
+"""NWS Hydrological Markup Language
+
+Attempt to break up the HML product into atomic data
+
+"""
+import pyiem.nws.product as product
+import re
+import datetime
+import pytz
+import pandas as pd
+import xml.etree.ElementTree as ET
+DELIMITER = """\<\?xml version="1.0" standalone="yes"\?\>"""
+
+
+def no999(val):
+    if val is None or val == -999:
+        return None
+    return val
+
+
+def parseUTC(s):
+    """Parse an ISO-ish string into UTC timestamp"""
+    if s is None:
+        return None
+    return datetime.datetime.strptime(s[:19],
+                                      "%Y-%m-%dT%H:%M:%S").replace(
+                                          tzinfo=pytz.timezone("UTC"))
+
+
+def parse_xml(token):
+    """Attempt to parse the XML into something useful"""
+    root = ET.fromstring(token)
+    hml = HMLData()
+    hml.station = root.attrib['id']
+    hml.originator = root.attrib.get('originator')
+    hml.generationtime = parseUTC(root.attrib['generationtime'])
+    for child in root:
+        if child.tag not in ['observed', 'forecast']:
+            continue
+        rows = []
+        for datum in child.findall("datum"):
+            secondary = datum.find('secondary')
+            rows.append(dict(name=child.tag,
+                             valid=parseUTC(datum.find('valid').text),
+                             primary=datum.find('primary').text,
+                             secondary=(secondary.text
+                                        if secondary is not None
+                                        else None)))
+        mydict = hml.data[child.tag]
+        df = pd.DataFrame(rows)
+        df['primary'] = pd.to_numeric(df['primary'], errors='coerse')
+        df['secondary'] = pd.to_numeric(df['secondary'], errors='coerse')
+        mydict['dataframe'] = df
+        mydict['issued'] = parseUTC(child.attrib.get('issued'))
+        for attr in ['primaryName', 'secondaryName',
+                     'primaryUnits', 'secondaryUnits']:
+            mydict[attr] = child.attrib.get(attr)
+    return hml
+
+
+class HMLData(object):
+
+    def __init__(self):
+        self.station = None
+        self.originator = None
+        self.generationtime = None
+        self.data = {'observed': dict(dataframe=None,
+                                      primaryUnits=None,
+                                      issued=None,
+                                      secondaryUnits=None,
+                                      primaryName=None,
+                                      secondaryName=None),
+                     'forecast': dict(dataframe=None,
+                                      primaryUnits=None,
+                                      issued=None,
+                                      secondaryUnits=None,
+                                      primaryName=None,
+                                      secondaryName=None)}
+
+
+class HML(product.TextProduct):
+    ''' Class for parsing and representing Space Wx Products '''
+
+    def __init__(self, text, utcnow=None, ugc_provider=None,
+                 nwsli_provider=None):
+        ''' constructor '''
+        product.TextProduct.__init__(self, text, utcnow=utcnow,
+                                     ugc_provider=ugc_provider,
+                                     nwsli_provider=nwsli_provider)
+        self.data = []
+        self.parsing()
+
+    def do_sql_observed(self, cursor, _hml):
+        """Process the observed portion of the dataset"""
+        fx = _hml.data['observed']
+        if fx['dataframe'] is None:
+            return
+        df = fx['dataframe']
+        if len(df.index) == 0:
+            return
+        minvalid = df['valid'].min()
+        maxvalid = df['valid'].max()
+        for col in ['primary', 'secondary']:
+            if fx[col+'Name'] is None:
+                continue
+            key = "%s[%s]" % (fx[col+'Name'], fx[col+'Units'])
+            cursor.execute("""DELETE from hml_observed_data WHERE
+            station = %s and valid >= %s and valid <= %s and
+            key = get_hml_observed_key(%s)
+            """, (_hml.station, minvalid, maxvalid, key))
+            for _, row in df.iterrows():
+                val = no999(row[col])
+                if val is None:
+                    continue
+                y = "%s" % (row['valid'].year,)
+                cursor.execute("""
+                    INSERT into hml_observed_data_""" + y + """
+                    (station, valid, key, value)
+                    VALUES (%s, %s, get_hml_observed_key(%s), %s)
+                    """, (_hml.station, row['valid'], key, val))
+
+    def do_sql_forecast(self, cursor, _hml):
+        """Process the forecast portion of the dataset"""
+        fx = _hml.data['forecast']
+        df = fx['dataframe']
+        if df is None:
+            return
+        if len(df.index) == 0:
+            return
+        # Get an id
+        cursor.execute("""
+        INSERT into hml_forecast(station, generationtime, originator,
+        product_id, primaryname, secondaryname, primaryunits,
+        secondaryunits, issued, forecast_sts, forecast_ets)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        RETURNING id
+        """, (_hml.station, _hml.generationtime,
+              _hml.originator, self.get_product_id(),
+              fx['primaryName'], fx['secondaryName'],
+              fx['primaryUnits'], fx['secondaryUnits'],
+              fx['issued'], df['valid'].min(), df['valid'].max()))
+        fid = cursor.fetchone()[0]
+        for _, row in fx['dataframe'].iterrows():
+            y = "%s" % (_hml.generationtime.year,)
+            cursor.execute("""
+                INSERT into hml_forecast_data_""" + y + """
+                (hml_forecast_id, valid, primary_value,
+                secondary_value) VALUES
+                (%s, %s, %s, %s)
+                """, (fid, row['valid'], no999(row['primary']),
+                      no999(row['secondary'])))
+
+    def sql(self, cursor):
+        """Persist this information to the database"""
+        for _hml in self.data:
+            self.do_sql_forecast(cursor, _hml)
+            self.do_sql_observed(cursor, _hml)
+
+    def parsing(self):
+        """Attempt to parse out what we have found"""
+        tokens = re.split(DELIMITER, self.unixtext)
+        for token in tokens:
+            if token.find("</site>") == -1:
+                continue
+            content = token.strip()
+            try:
+                self.data.append(parse_xml(content))
+            except Exception as exp:
+                self.warnings.append(("Parsing %s resulted in %s\n%s"
+                                      ) % (self.get_product_id(), exp,
+                                           content))
+
+    def __str__(self):
+        """string representation"""
+        s = "HML %s\n" % (self.get_product_id(),)
+        for _hml in self.data:
+            s += "  + SID: %s generationTime: %s\n" % (_hml.station,
+                                                       _hml.generationtime)
+        return s
+
+
+def parser(buf, utcnow=None, ugc_provider=None, nwsli_provider=None):
+    """Parse a HML NOAAPort product
+
+    This may have multiple xml documents inside.
+
+    Args:
+      buf (str): What we want to parse
+    """
+    return HML(buf, utcnow, ugc_provider, nwsli_provider)
---
+++ pyiem/nws/products/tests/test_hml.py
@@ -0,0 +1,42 @@
+import os
+import datetime
+import pytz
+import psycopg2.extras
+import unittest
+from pyiem.nws.products.hml import parser as hmlparser
+
+
+def get_file(name):
+    ''' Helper function to get the text file contents '''
+    basedir = os.path.dirname(__file__)
+    fn = "%s/../../../../data/product_examples/%s" % (basedir, name)
+    return open(fn).read()
+
+
+def utc(year, month, day, hour=0, minute=0):
+    """UTC Timestamp generator"""
+    return datetime.datetime(year, month, day, hour, minute).replace(
+                        tzinfo=pytz.timezone("UTC"))
+
+
+class TestProducts(unittest.TestCase):
+    """ Tests """
+    def setUp(self):
+        ''' This is called for each test, beware '''
+        self.dbconn = psycopg2.connect(database='hads')
+        # Note the usage of RealDictCursor here, as this is what
+        # pyiem.twistedpg uses
+        self.txn = self.dbconn.cursor(
+            cursor_factory=psycopg2.extras.DictCursor)
+
+    def tearDown(self):
+        ''' This is called after each test, beware '''
+        self.dbconn.rollback()
+        self.dbconn.close()
+
+    def test_160826_hmlarx(self):
+        """Lets dance"""
+        prod = hmlparser(get_file("HMLARX.txt"))
+        prod.sql(self.txn)
+        self.assertEquals(len(prod.warnings), 0,
+                          '\n'.join(prod.warnings))
