13c38a10523eb353a1c137351edcbed260dd5200
akrherz <akrherz@iastate.edu>
2018-03-20 11:29:13 -0500

    allow ACM as invalid, but valid WMO header

--- pyiem/nws/product.py
+++ pyiem/nws/product.py
@@ -1,7 +1,6 @@
 """Base Class encapsulating a NWS Text Product"""
 import datetime
 import re
-import sys
 
 import pytz
 from shapely.geometry import Polygon, MultiPolygon
@@ -23,7 +22,7 @@ TIME_RE = re.compile(("^([0-9:]+) (AM|PM) ([A-Z][A-Z][A-Z]?T) "
 # Note that bbb of RTD is supported here, but does not appear to be allowed
 WMO_RE = re.compile(("^(?P<ttaaii>[A-Z0-9]{4,6}) (?P<cccc>[A-Z]{4}) "
                      r"(?P<ddhhmm>[0-3][0-9][0-2][0-9][0-5][0-9])\s*"
-                     r"(?P<bbb>[ACR][ACORT][A-Z])?\s*$"), re.M)
+                     r"(?P<bbb>[ACR][ACMORT][A-Z])?\s*$"), re.M)
 TIME_MOT_LOC = re.compile((r"TIME\.\.\.MOT\.\.\.LOC\s+(?P<ztime>[0-9]{4})Z\s+"
                            r"(?P<dir>[0-9]{1,3})DEG\s+"
                            r"(?P<sknt>[0-9]{1,3})KT\s+(?P<loc>[0-9 ]+)"))
