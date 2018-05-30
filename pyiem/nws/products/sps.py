79df314192b8e3a5838c3eac606062dc18f071f4
akrherz <akrherz@iastate.edu>
2018-05-30 09:59:48 -0500

    introduce parser for Special Weather Statements (SPS)

---
+++ pyiem/nws/products/sps.py
@@ -0,0 +1,103 @@
+"""Special Weather Statement"""
+import re
+import datetime
+
+from shapely.geometry import MultiPolygon
+from pyiem import reference
+from pyiem.nws.product import TextProduct
+from pyiem.nws.ugc import ugcs_to_text
+
+SPECIAL_WX_STATEMENT = re.compile("SPECIAL WEATHER STATEMENT", re.I)
+TILL = re.compile(" (TILL|UNTIL|THROUGH) [0-9]{1,2}:?[0-5][0-9]", re.I)
+
+
+def dedup_headline(headline, ugcs, counties, expire):
+    """Try to not be redundant
+
+    Args:
+      headline (str): our current headline
+      ugcs (list(ugc)): list of ugcs this SPS is for
+      counties (str): our current parsed string of counties
+      expire (str): our current parsed expiration string
+
+    Returns:
+       (str): our new counties, which has been deduped
+       (str): our new expire, which has been deduped
+    """
+    if TILL.search(headline):
+        expire = ""
+    if ugcs:
+        hits = 0
+        for ugc in ugcs:
+            if ugc.name is None:
+                continue
+            if headline.upper().find(ugc.name.upper()) > -1:
+                hits += 1
+        if (float(hits) / float(len(ugcs))) > 0.7:
+            counties = ""
+
+    return counties, expire
+
+
+class SPSProduct(TextProduct):
+    """A Special Weather Statement"""
+
+    def sql(self, txn):
+        """Do database save in the case of a polygon"""
+        for seg in self.segments:
+            if not seg.sbw:
+                continue
+            # Figure out an end time
+            ets = self.valid + datetime.timedelta(hours=1)
+            if seg.ugcexpire is not None:
+                ets = seg.ugcexpire
+            giswkt = 'SRID=4326;%s' % (MultiPolygon([seg.sbw]).wkt,)
+            sql = """
+                INSERT into text_products(product, product_id, geom,
+                issue, expire, pil) values (%s, %s, %s, %s, %s, %s)
+            """
+            myargs = (self.unixtext, self.get_product_id(), giswkt,
+                      self.valid, ets, self.afos)
+            txn.execute(sql, myargs)
+
+    def get_jabbers(self, uri, uri2=None):
+        """return the standard [[text, html, xtra], ] for jabber"""
+        res = []
+        xtra = {'product_id': self.get_product_id()}
+        for seg in self.segments:
+            # Skip any segments that don't have UGC information
+            if not seg.ugcs:
+                continue
+            headline = "[No headline was found in SPS]"
+            if seg.headlines:
+                headline = (seg.headlines[0]).replace("\n", " ")
+            elif SPECIAL_WX_STATEMENT.search(seg.unixtext):
+                headline = "Special Weather Statement"
+            counties = " for %s" % (ugcs_to_text(seg.ugcs), )
+            expire = ""
+            if seg.ugcexpire is not None:
+                expire = " till %s %s" % (
+                    (seg.ugcexpire -
+                     datetime.timedelta(hours=reference.offsets.get(self.z, 0))
+                     ).strftime("%-I:%M %p"), self.z)
+            counties, expire = dedup_headline(headline, seg.ugcs, counties,
+                                              expire)
+            xtra['channels'] = self.afos
+            mess = ("%s issues %s%s%s %s?pid=%s"
+                    ) % (self.source[1:], headline, counties,
+                         expire, uri, xtra['product_id'])
+            htmlmess = ("<p>%s issues <a href='%s?pid=%s'>%s</a>%s%s</p>"
+                        ) % (self.source[1:], uri, xtra['product_id'],
+                             headline, counties, expire)
+            xtra['twitter'] = "%s%s%s %s?pid=%s" % (headline, counties,
+                                                    expire, uri,
+                                                    xtra['product_id'])
+            res.append([mess, htmlmess, xtra])
+
+        return res
+
+
+def parser(text, utcnow=None, ugc_provider=None, nwsli_provider=None):
+    """The SPS Parser"""
+    return SPSProduct(text, utcnow, ugc_provider=ugc_provider,
+                      nwsli_provider=nwsli_provider)
---
+++ pyiem/nws/products/tests/test_sps.py
@@ -0,0 +1,41 @@
+"""SPS Parsing"""
+import os
+
+from psycopg2.extras import RealDictCursor
+from pyiem.util import get_dbconn
+from pyiem.nws.ugc import UGC
+from pyiem.nws.products.sps import parser as spsparser
+
+
+def get_transaction():
+    """Get a cursor we can use"""
+    pgconn = get_dbconn('postgis')
+    return pgconn.cursor(cursor_factory=RealDictCursor)
+
+
+def get_file(name):
+    """Helper function to get the text file contents"""
+    basedir = os.path.dirname(__file__)
+    fn = "%s/../../../../data/product_examples/%s" % (basedir, name)
+    return open(fn).read()
+
+
+def test_sps():
+    """Can we parse a SPS properly, yes we can!"""
+    txn = get_transaction()
+    ugc_provider = {'ALZ039': UGC("AL", "Z", "039", name='Marengo')}
+    prod = spsparser(get_file('SPSBMX.txt'), ugc_provider=ugc_provider)
+    jmsgs = prod.get_jabbers('http://localhost')
+    assert len(prod.segments) == 2
+    assert len(jmsgs) == 1
+    expected = ("<p>BMX issues <a href='http://localhost?pid=201805292152-"
+                "KBMX-WWUS84-SPSBMX'>SIGNIFICANT WEATHER ADVISORY FOR "
+                "SOUTHWESTERN MARENGO COUNTY UNTIL 515 PM CDT</a></p>")
+    assert jmsgs[0][1] == expected
+    assert jmsgs[0][2]['channels'] == 'SPSBMX'
+
+    prod.sql(txn)
+    txn.execute("""
+        SELECT count(*) from text_products where product_id = %s
+    """, (prod.get_product_id(),))
+    assert txn.fetchall()[0]['count'] == 1
