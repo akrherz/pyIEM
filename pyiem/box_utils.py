8a52305b0c72d0f22b8e4dbce16bb90779bf32d7
akrherz <akrherz@iastate.edu>
2018-09-04 13:58:16 -0500

    allow for overwrite of box_client uploads

--- pyiem/box_utils.py
+++ pyiem/box_utils.py
@@ -15,14 +15,16 @@ def _store_tokens(access_token, refresh_token):
     set_property('boxclient.refresh_token', refresh_token)
 
 
-def sendfiles2box(remotepath, filenames, remotefilenames=None):
-    """Send a file(s) to Box
+def sendfiles2box(remotepath, filenames, remotefilenames=None,
+                  overwrite=False):
+    """Send a file(s) to Box.
 
     Args:
       remotepath (str): remote directory to send file(s) to
       filenames (str or list): local files to send to box
       remotefilenames (str or list, optional): same size as filenames and
         optional as to if they should have different names or not
+      overwrite (bool): should this overwrite existing files, default `False`
 
     Returns:
       list of ids of the uploaded content
@@ -49,7 +51,7 @@ def sendfiles2box(remotepath, filenames, remotefilenames=None):
         offset = 0
         found = False
         while not found:
-            LOG.debug("folder(%s).get_items(offset=%s)" % (folder_id, offset))
+            LOG.debug("folder(%s).get_items(offset=%s)", folder_id, offset)
             items = client.folder(
                 folder_id=folder_id).get_items(limit=100, offset=offset)
             for item in items:
@@ -62,23 +64,39 @@ def sendfiles2box(remotepath, filenames, remotefilenames=None):
                 break
             offset += 100
         if not found:
-            LOG.debug("Creating folder %s inside of %s" % (token, folder_id))
+            LOG.debug("Creating folder %s inside of %s", token, folder_id)
             item = client.folder(folder_id=folder_id).create_subfolder(token)
             folder_id = item.id
-    LOG.debug("Now we upload to folder_id: %s" % (folder_id, ))
+    LOG.debug("Now we upload to folder_id: %s", folder_id)
     res = []
     for localfn, remotefn in zip(filenames, remotefilenames):
-        LOG.debug("uploading %s" % (localfn, ))
+        LOG.debug("uploading %s", localfn)
         try:
             item = client.folder(folder_id=folder_id).upload(localfn, remotefn)
             res.append(item.id)
         except Exception as exp:
-            LOG.debug(("Upload of %s resulted in exception: %s"
-                       ) % (localfn, exp))
+            if overwrite and hasattr(exp, 'context_info'):
+                _fileid = exp.context_info['conflicts']['id']
+                LOG.info("overwriting %s fid: %s", remotefn, _fileid)
+                try:
+                    item = client.file(_fileid).update_contents(localfn)
+                    res.append(_fileid)
+                    continue
+                except Exception as exp2:
+                    LOG.debug(
+                        "Upload_Contents of %s resulted in exception: %s",
+                        localfn, exp2
+                    )
+                    continue
+            LOG.debug(
+                "Upload of %s resulted in exception: %s", localfn, exp
+            )
             res.append(None)
 
     return res
 
 
 if __name__ == '__main__':
-    sendfiles2box("/bah/bah/bah", "/tmp/out.mpg")
+    LOG.setLevel(logging.DEBUG)
+    LOG.addHandler(logging.StreamHandler())
+    sendfiles2box("/bah/bah/bah", "/tmp/z_01my18.dbf", overwrite=True)
