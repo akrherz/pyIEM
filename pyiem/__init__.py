caaa876bf431ff3ae3f2e423b0f65d07bc57841f
akrherz <akrherz@iastate.edu>
2019-01-18 22:16:45 -0600

    denote pyIEM release 0.10.0

--- pyiem/__init__.py
+++ pyiem/__init__.py
@@ -4,4 +4,4 @@ Python is an important part of the Iowa Environmental Mesonet (IEM).  This
 package is used by many parts of the IEM codebase and hopefully somewhat
 useful to others!?!?
 """
-__version__ = '0.9.1'
+__version__ = '0.10.0'
