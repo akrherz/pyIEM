8c914962a85da82d646c941af7b0fb33ab561cf7
daryl herzmann <akrherz@iastate.edu>
2019-03-13 13:32:27 -0500

    rework HML database logic to not insert null values (#93)

--- pyiem/nws/products/tests/test_hml.py
+++ pyiem/nws/products/tests/test_hml.py
@@ -1,10 +1,10 @@
 """HML"""
 from __future__ import print_function
 import os
-import unittest
 import datetime
 
 import psycopg2.extras
+import pytest
 from pyiem.nws.products.hml import parser as hmlparser
 from pyiem.util import get_dbconn
 
@@ -12,41 +12,45 @@ from pyiem.util import get_dbconn
 def get_file(name):
     ''' Helper function to get the text file contents '''
     basedir = os.path.dirname(__file__)
-    fn = "%s/../../../../data/product_examples/%s" % (basedir, name)
+    fn = "%s/../../../../data/product_examples/HML/%s" % (basedir, name)
     return open(fn).read()
 
 
-class TestHML(unittest.TestCase):
-    """ Tests """
-    def setUp(self):
-        ''' This is called for each test, beware '''
-        self.dbconn = get_dbconn('hads')
-        # Note the usage of RealDictCursor here, as this is what
-        # pyiem.twistedpg uses
-        self.txn = self.dbconn.cursor(
-            cursor_factory=psycopg2.extras.DictCursor)
+@pytest.fixture
+def dbcursor():
+    """Get database conn."""
+    dbconn = get_dbconn('hads')
+    # Note the usage of RealDictCursor here, as this is what
+    # pyiem.twistedpg uses
+    return dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    def tearDown(self):
-        ''' This is called after each test, beware '''
-        self.dbconn.rollback()
-        self.dbconn.close()
 
-    def test_160826_hmlarx(self):
-        """Lets dance"""
-        prod = hmlparser(get_file("HMLARX.txt"))
-        prod.sql(self.txn)
-        self.assertEquals(len(prod.warnings), 0,
-                          '\n'.join(prod.warnings))
+def test_190313_missingstage(dbcursor):
+    """Figure out why this HML is missing stage info."""
+    prod = hmlparser(get_file("HMLDMX.txt"))
+    assert not prod.warnings
+    prod.sql(dbcursor)
+    dbcursor.execute("""
+        SELECT * from hml_observed_data_2019 WHERE station = 'JANI4'
+        and valid > '2019-03-13' and valid < '2019-03-14'
+    """)
+    assert dbcursor.rowcount == 8
 
-        self.assertEquals(prod.data[0].stationname,
-                          "CEDAR RIVER 2 S St. Ansgar")
 
-    def test_161010_timing(self):
-        """test how fast we can parse the file, over and over again"""
-        sts = datetime.datetime.now()
-        for _ in range(100):
-            _ = hmlparser(get_file("HMLARX.txt"))
-        ets = datetime.datetime.now()
-        rate = (ets - sts).total_seconds() / 100.
-        print("sec per parse %.4f" % (rate,))
-        self.assertTrue(rate < 1.)
+def test_160826_hmlarx(dbcursor):
+    """Lets dance"""
+    prod = hmlparser(get_file("HMLARX.txt"))
+    prod.sql(dbcursor)
+    assert not prod.warnings
+    assert prod.data[0].stationname == "CEDAR RIVER 2 S St. Ansgar"
+
+
+def test_161010_timing():
+    """test how fast we can parse the file, over and over again"""
+    sts = datetime.datetime.now()
+    for _ in range(100):
+        hmlparser(get_file("HMLARX.txt"))
+    ets = datetime.datetime.now()
+    rate = (ets - sts).total_seconds() / 100.
+    print("sec per parse %.4f" % (rate,))
+    assert rate < 1.
