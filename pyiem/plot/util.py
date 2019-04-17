3ac80ba71b0a96d62a5fb30585d5f204de592b1d
akrherz <akrherz@iastate.edu>
2019-04-16 22:24:33 -0500

    need additional slop factor

--- pyiem/plot/util.py
+++ pyiem/plot/util.py
@@ -5,10 +5,11 @@ def fitbox(fig, text, x0, x1, y0, y1, **kwargs):
     """Fit text into a NDC box."""
     figbox = fig.get_window_extent().transformed(
         fig.dpi_scale_trans.inverted())
-    px0 = x0 * fig.dpi * figbox.width
-    px1 = x1 * fig.dpi * figbox.width
-    py0 = y0 * fig.dpi * figbox.height
-    py1 = y1 * fig.dpi * figbox.height
+    # need some slop for decimal comparison below
+    px0 = x0 * fig.dpi * figbox.width - 0.15
+    px1 = x1 * fig.dpi * figbox.width + 0.15
+    py0 = y0 * fig.dpi * figbox.height - 0.15
+    py1 = y1 * fig.dpi * figbox.height + 0.15
     # print("px0: %s px1: %s py0: %s py1: %s" % (px0, px1, py0, py1))
     xanchor = x0
     if kwargs.get('ha', '') == 'center':
@@ -25,6 +26,7 @@ def fitbox(fig, text, x0, x1, y0, y1, **kwargs):
     for fs in range(50, 1, -2):
         txt.set_fontsize(fs)
         tbox = txt.get_window_extent(fig.canvas.get_renderer())
+        # print("fs: %s tbox: %s" % (fs, str(tbox)))
         if (tbox.x0 >= px0 and tbox.x1 < px1 and tbox.y0 >= py0 and
                 tbox.y1 <= py1):
             break
