c038d12a8afa7295afa451cb224dd8da3d614a88
akrherz <akrherz@iastate.edu>
2019-05-15 13:53:48 -0500

    use frame filling cornbelt by default

--- pyiem/reference.py
+++ pyiem/reference.py
@@ -51,7 +51,7 @@ CONUS_SOUTH = 24.47
 
 SECTORS = {
     'midwest': [MW_WEST, MW_EAST, MW_SOUTH, MW_NORTH],
-    'cornbelt': [-99., -84., 38.5, 44.],
+    'cornbelt': [-99., -84., 37.17, 45.2],
 }
 
 txt2drct = {
