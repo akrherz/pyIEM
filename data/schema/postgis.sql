e3526ec14999c3c93a27965fdb40cde62548a88a
akrherz <akrherz@iastate.edu>
2019-07-05 15:07:08 -0500

    fix redefined warnings_2015 table

--- data/schema/postgis.sql
+++ data/schema/postgis.sql
@@ -2165,15 +2165,6 @@ GRANT SELECT on raob_profile_2015 to nobody,apache;
 CREATE INDEX raob_profile_2015_fid_idx 
 	on raob_profile_2015(fid);
 
-CREATE TABLE warnings_2015() inherits (warnings);
-CREATE INDEX warnings_2015_combo_idx on 
-	warnings_2015(wfo, phenomena, eventid, significance);
-CREATE INDEX warnings_2015_expire_idx on warnings_2015(expire);
-CREATE INDEX warnings_2015_gtype_idx on warnings_2015(gtype);
-CREATE INDEX warnings_2015_issue_idx on warnings_2015(issue);
-CREATE INDEX warnings_2015_ugc_idx on warnings_2015(ugc);
-CREATE INDEX warnings_2015_wfo_idx on warnings_2015(wfo);
-grant select on warnings_2015 to nobody,apache;
 
 CREATE table sbw_2015() inherits (sbw);
 create index sbw_2015_idx on sbw_2015(wfo,eventid,significance,phenomena);
