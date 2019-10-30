fix y-elevation calculation in dep#read_slp

src/pyiem/dep.py
@@ -111,7 +111,7 @@ def read_yld(filename):
 
 
 def read_slp(filename):
-    """read WEPP slp file
+    """read WEPP slp file.
 
     Args:
       filename (str): Filename to read
@@ -125,17 +125,22 @@ def read_slp(filename):
     xpos = 0
     elev = 0
     for seg in range(segments):
-        line1 = lines[7 + seg * 2]
-        (_pts, length) = [float(x) for x in line1.split()]
+        # first line is pts and x-length
+        (_pts, length) = [float(x) for x in lines[7 + seg * 2].split()]
+        # next line is the combo of x-position along length and slope at pt
         line2 = lines[8 + seg * 2].replace(",", "")
         tokens = np.array([float(x) for x in line2.split()])
+        # first value in each pair is the x-pos relative to the length
         xs = xpos + tokens[::2] * length
+        # second value is the slope at that position?
         slopes = tokens[1::2]
+        # initialize the y-position at the current elevation
         ys = [elev]
         for i in range(1, len(slopes)):
-            elev -= (xs[i] - xs[0]) * slopes[i - 1]
+            # dx * slope
+            elev -= (xs[i] - xs[i - 1]) * slopes[i - 1]
             ys.append(elev)
-        res[seg] = {"x": xs, "slopes": slopes, "y": ys}
+        res[seg] = {"x": xs, "slopes": slopes, "y": np.array(ys)}
         xpos += length
     return res
 