b6b7e16901129c0e97e468227963932e404e255a
akrherz <akrherz@users.noreply.github.com>
2020-07-29 19:23:59 +0000

    Deploying to gh-pages from master @ ad2f6d3a67dbe5aa7db195494c13e4fafa2b9e1a 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1+g1dc4215',
+    VERSION: '0.0.post1+gad2f6d3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
