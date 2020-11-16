d88fcb208aaa3da7d8238fd95204c8294e390ecf
akrherz <akrherz@iastate.edu>
2020-11-16 08:53:42 -0600

    complete test coverage of pyiem.nws.vtec

--- tests/nws/products/test_products_vtec.py
+++ tests/nws/products/test_products_vtec.py
@@ -68,6 +68,22 @@ def test_issue284_incomplete_update(dbcursor):
     assert warnings[0].startswith(CUGC)
 
 
+def test_201116_timezone():
+    """Test what happens with some timezone fun."""
+    valid = utc(2020, 9, 9, 0, 29)
+    prod = vtecparser(get_test_file("SVR/SVRPSR.txt"), utcnow=valid)
+    ans = "valid at 5:29 PM MST"
+    assert prod.segments[0].vtec[0].get_begin_string(prod) == ans
+
+
+def test_vtec_begin_time_format():
+    """Test that we can call vtec begin_time."""
+    valid = utc(2010, 4, 5, 21, 47)
+    prod = vtecparser(get_test_file("SVRLMK.txt"), utcnow=valid)
+    ans = "valid at 5:47 PM EDT"
+    assert prod.segments[0].vtec[0].get_begin_string(prod) == ans
+
+
 def test_old_windhail_tag():
     """Test that we can parse legacy wind...hail tags."""
     valid = utc(2010, 4, 5, 21, 47)
