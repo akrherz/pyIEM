c91a0d326ac26e0cc1868808ae2866c3aa43fb3e
akrherz <akrherz@iastate.edu>
2021-09-20 15:28:37 +0000

    Deploying to gh-pages from main @ 46d6bf50dab379f51a6c0dfc21421670bdc64e35 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1+g388ce11',
+    VERSION: '0.0.post1+g46d6bf5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
