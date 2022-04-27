f257b6bc888ae25afafaab00541fa548d4d5e397
akrherz <akrherz@iastate.edu>
2022-04-26 21:58:00 -0500

    refactor windrose_utils add limit_by_doy kwarg

--- src/pyiem/windrose_utils.py
+++ src/pyiem/windrose_utils.py
@@ -11,10 +11,14 @@ import numpy as np
 import pandas as pd
 from pandas import read_sql
 from metpy.units import units as mpunits
+from sqlalchemy import text
+
+# Local
 from pyiem.plot.util import fitbox
 from pyiem.plot.windrose import histogram, plot, WindrosePlot
-from pyiem.util import get_dbconnstr, utc
+from pyiem.util import get_sqlalchemy_conn, utc
 from pyiem.network import Table as NetworkTable
+
 
 WINDUNITS = {
     "mph": {
@@ -46,32 +50,39 @@ RAOB_BINS = {
 }
 
 
-def _get_timeinfo(arr, datepart, fullsize, tzname):
-    """Convert the months/hours array provided into label text and SQL
+def _make_timelimit_string(kwargs):
+    """Generate a string for the time limiters"""
+    hours = kwargs.get("hours")
+    months = kwargs.get("months")
+    limit_by_doy = kwargs.get("limit_by_doy")
+    if hours is None and months is None and limit_by_doy is None:
+        return ""
+    tlimit = "[Time Domain: "
+    if limit_by_doy is not None:
+        sts = kwargs.get("sts")
+        ets = kwargs.get("ets")
+        d1 = sts.strftime("%b %-d")
+        d2 = ets.strftime("%b %-d")
+        if sts.strftime("%m%d") > ets.strftime("%m%d"):
+            d1, d2 = d2, d1
+        tlimit += f"{d1} - {d2}"
+    elif months is not None and len(months) < 12:
+        for h in months:
+            tlimit += f"{month_abbr[h]},"
+    if hours is not None:
+        if len(hours) > 4:
+            tlimit += (
+                f"{datetime(2000, 1, 1, hours[0]):%-I %p}-"
+                f"{datetime(2000, 1, 1, hours[-1]):%-I %p}"
+            )
+        else:
+            for h in hours:
+                tlimit += f"{datetime(2000, 1, 1, h):%-I %p},"
+    tlimit += "]"
+    return tlimit
 
-    Args:
-      arr (list): A list of ints
-      datepart (str): the part to extract from the database timestamp
-      fullsize (int): the size of specifying all dates
-      tzname (str): The timezone to compute this limiter in.
 
-    Returns:
-      dict with keys `sqltext` and `labeltext`
-    """
-    sql = ""
-    lbl = "All included"
-    te = "" if tzname is None else f" at time zone '{tzname}'"
-    if len(arr) == 1:
-        sql = f" and extract({datepart} from valid{te}) = {arr[0]} "
-        lbl = str(tuple(arr))
-    elif len(arr) < fullsize:
-        ta = str(tuple(arr)).replace("'", "")
-        sql = f" and extract({datepart} from valid{te}) in {ta} "
-        lbl = str(tuple(arr))
-    return dict(sqltext=sql, labeltext=lbl)
-
-
-def _get_data(station, database, sts, ets, monthinfo, hourinfo, level):
+def _get_data(station, **kwargs):
     """Helper function to get data out of IEM databases
 
     Args:
@@ -87,61 +98,77 @@ def _get_data(station, database, sts, ets, monthinfo, hourinfo, level):
     Returns:
       pandas.DataFrame of the data
     """
-    # Query observations
-    db = get_dbconnstr(database, user="nobody")
     rlimiter = ""
+    sts = kwargs.get("sts")
+    ets = kwargs.get("ets")
+    database = kwargs.get("database", "asos")
     if database == "asos":
         rlimiter = " and report_type = 2 "
-    sql = (
+    tlimit = ""
+    sqlargs = {}
+    if kwargs.get("limit_by_doy", False):
+        tlimit = (
+            f"and to_char(valid, 'mmdd') >= '{sts:%m%d}' and "
+            f"to_char(valid, 'mmdd') < '{ets:%m%d}' "
+        )
+        if sts.strftime("%m%d") > ets.strftime("%m%d"):
+            tlimit = (
+                f"and (to_char(valid, 'mmdd') >= '{ets:%m%d}' or "
+                f"to_char(valid, 'mmdd') < '{sts:%m%d}') "
+            )
+    elif kwargs.get("months") is not None and len(kwargs["months"]) < 12:
+        sqlargs["months"] = tuple(kwargs["months"])
+        tlimit += " and extract(month from valid) in :months "
+    if kwargs.get("hours") is not None:  # can combo with the above
+        tzname = kwargs.get("tzname")
+        te = "" if tzname is None else f" at time zone '{tzname}'"
+        if len(kwargs["hours"]) < 24:
+            sqlargs["hours"] = tuple(kwargs["hours"])
+            tlimit += f" and extract(hour from valid{te}) in :hours "
+    sql = text(
         "SELECT sknt, drct, valid at time zone 'UTC' as valid "
-        f"from alldata WHERE station = '{station}' "
-        f"and valid > '{sts}' and valid < '{ets}' {monthinfo['sqltext']} "
-        f"{hourinfo['sqltext']} {rlimiter}"
+        "from alldata WHERE station = :station "
+        f"and valid > :sts and valid < :ets {tlimit} {rlimiter}"
     )
-    if level is not None:  # HACK!
-        db = get_dbconnstr("raob")
+    sqlargs["station"] = station
+    sqlargs["sts"] = sts
+    sqlargs["ets"] = ets
+    if kwargs.get("level") is not None:  # HACK!
+        database = "raob"
         # here comes another hack, stations with starting with _ are virtual
-        stations = [station, "ZZZZ"]
+        sqlargs["stations"] = [station, "ZZZZ"]
         if station.startswith("_"):
             nt = NetworkTable("RAOB")
-            stations = (
+            sqlargs["stations"] = tuple(
                 nt.sts.get(station, {})
                 .get("name", "X--YYY ZZZ")
                 .split("--")[1]
                 .strip()
                 .split(" ")
             )
-        sql = f"""SELECT p.smps * 1.94384 as sknt, p.drct,
+        sql = text(
+            f"""SELECT p.smps * 1.94384 as sknt, p.drct,
         f.valid at time zone 'UTC' as valid from
         raob_flights f JOIN raob_profile p on (f.fid = p.fid) WHERE
-        f.station in {str(tuple(stations))} and p.pressure = {level} and
+        f.station in :stations and p.pressure = :level and
         p.smps is not null
-        and p.drct is not null and valid >= '{sts}' and valid < '{ets}'
-        {monthinfo['sqltext']} {hourinfo['sqltext']}
-        """
-    df = read_sql(sql, db, index_col=None)
+        and p.drct is not null and valid >= :sts and valid < :ets
+        {tlimit}"""
+        )
+        sqlargs["level"] = kwargs["level"]
+    with get_sqlalchemy_conn(database) as conn:
+        df = read_sql(sql, conn, params=sqlargs, index_col=None)
     if not df.empty:
         # Make valid column timezone aware
         df["valid"] = df["valid"].dt.tz_localize(timezone.utc)
     # If sknt or drct are null, we want to set the other to null as well
-    df.loc[pd.isnull(df["drct"]), "sknt"] = None
-    df.loc[pd.isnull(df["sknt"]), "drct"] = None
+    df.loc[pd.isnull(df["drct"]), "sknt"] = np.nan
+    df.loc[pd.isnull(df["sknt"]), "drct"] = np.nan
 
     return df
 
 
-def _make_textresult(
-    station,
-    df,
-    units,
-    nsector,
-    sname,
-    monthinfo,
-    hourinfo,
-    level,
-    bins,
-    tzname,
-):
+def _make_textresult(station, df, **kwargs):
     """Generate a text table of windrose information
 
     Args:
@@ -160,19 +187,20 @@ def _make_textresult(
       str of information"""
     if df.empty:
         return "No Data Found"
-    wu = WINDUNITS[units]
+    wu = WINDUNITS[kwargs.get("units", "mph")]
+    bins = kwargs.get("bins")
     if not hasattr(bins, "units"):
         bins = wu["bins"] * wu["units"]
-        if level is not None:
-            bins = RAOB_BINS[units] * wu["units"]
+        if kwargs.get("level") is not None:
+            bins = RAOB_BINS[kwargs.get("units", "mph")] * wu["units"]
     # Effectively filters out the nulls
     df2 = df[df["drct"] >= 0]
     speed = df2["sknt"].values * mpunits("knots")
     direction = df2["drct"].values * mpunits("degree")
     calm_percent, dir_centers, table = histogram(
-        speed, direction, bins, nsector
+        speed, direction, bins, kwargs.get("nsector", 36)
     )
-    sn = sname if sname is not None else f"(({station}))"
+    sn = kwargs.get("sname", f"(({station}))")
     res = (
         "# Windrose Data Table (Percent Frequency) " f"for {sn} ({station})\n"
     )
@@ -180,12 +208,11 @@ def _make_textresult(
         f"# Observations Used/Missing/Total: {len(df2.index)}/"
         f"{len(df.index) - len(df2.index)}/{len(df.index)}\n"
     )
-    res += f"# {_time_domain_string(df, tzname)}\n"
-    res += f"# Hour Limiter: {hourinfo['labeltext']}\n"
-    res += f"# Month Limiter: {monthinfo['labeltext']}\n"
+    res += f"# {_time_domain_string(df, kwargs.get('tzname'))}\n"
+    res += f"# Time Limiter: {_make_timelimit_string(kwargs)}\n"
     res += f"# Wind Speed Units: {wu['label']}\n"
-    if level is not None:
-        res += f"# RAOB Pressure (hPa) Level: {level}\n"
+    if kwargs.get("level") is not None:
+        res += f"# RAOB Pressure (hPa) Level: {kwargs['level']}\n"
     res += (
         f"# Generated {utc():%d %b %Y %H:%M} UTC, "
         "contact: akrherz@iastate.edu\n"
@@ -233,20 +260,7 @@ def _time_domain_string(df, tzname):
     )
 
 
-def _make_plot(
-    station,
-    df,
-    units,
-    nsector,
-    rmax,
-    hours,
-    months,
-    sname,
-    level,
-    bins,
-    tzname,
-    **kwargs,
-):
+def _make_plot(station, df, **kwargs):
     """Generate a matplotlib windrose plot
 
     Args:
@@ -267,7 +281,7 @@ def _make_plot(
     Returns:
       matplotlib.Figure
     """
-    wu = WINDUNITS[units]
+    wu = WINDUNITS[kwargs.get("units", "mph")]
     # Filters the missing values
     df2 = df[df["drct"] >= 0]
     direction = df2["drct"].values * mpunits("degree")
@@ -275,10 +289,11 @@ def _make_plot(
         speed = df2["speed"].values * wu["units"]
     else:
         speed = df2["sknt"].values * mpunits("knots")
+    bins = kwargs.get("bins")
     if not hasattr(bins, "units"):
         bins = wu["bins"] * wu["units"]
-        if level is not None:
-            bins = RAOB_BINS[units] * wu["units"]
+        if kwargs.get("level") is not None:
+            bins = RAOB_BINS[kwargs.get("units", "mph")] * wu["units"]
     if len(df2.index) < 5:
         wp = WindrosePlot()
         wp.ax.text(
@@ -293,42 +308,27 @@ def _make_plot(
         direction,
         speed,
         bins=bins,
-        nsector=nsector,
-        rmax=rmax,
+        nsector=kwargs.get("nsector", 36),
+        rmax=kwargs.get("rmax"),
         cmap=kwargs.get("cmap"),
     )
 
     # Now we put some fancy debugging info on the plot
-    tlimit = "[Time Domain: "
-    if len(hours) == 24 and len(months) == 12:
-        tlimit = ""
-    if len(hours) < 24:
-        if len(hours) > 4:
-            tlimit += (
-                f"{datetime(2000, 1, 1, hours[0]):%-I %p}-"
-                f"{datetime(2000, 1, 1, hours[-1]):%-I %p}"
-            )
-        else:
-            for h in hours:
-                tlimit += f"{datetime(2000, 1, 1, h):%-I %p},"
-    if len(months) < 12:
-        for h in months:
-            tlimit += f"{month_abbr[h]},"
-    if tlimit != "":
-        tlimit += "]"
-    sn = sname if sname is not None else f"(({station}))"
+    tlimit = _make_timelimit_string(kwargs)
+    sn = kwargs.get("sname", f"(({station}))")
+    level = kwargs.get("level")
     sl = "" if level is None else f" @{level} hPa"
     label = (
         f"[{station}] {sn}{sl}\n"
         f"Windrose Plot {tlimit}\n"
-        f"Time Bounds: {_time_domain_string(df, tzname)}"
+        f"Time Bounds: {_time_domain_string(df, kwargs.get('tzname'))}"
     )
     fitbox(wp.fig, label, 0.14, 0.99, 0.92, 0.99, ha="left")
     label = (
         "Summary\n"
         f"obs count: {len(df.index)}\n"
         f"Missing: {len(df.index) - len(df2.index)}\n"
-        f"Avg Speed: {speed.m.mean():.1f} {units}"
+        f"Avg Speed: {speed.m.mean():.1f} {kwargs.get('units', 'mph')}"
     )
     wp.fig.text(0.96, 0.11, label, ha="right", fontsize=14)
     if not kwargs.get("nogenerated", False):
@@ -341,7 +341,7 @@ def _make_plot(
         )
     # Denote the direction blowing from
     lbl = (
-        f"Calm values are < {bins.m[0]:.1f} {units}\n"
+        f"Calm values are < {bins.m[0]:.1f} {kwargs.get('units', 'mph')}\n"
         "Arrows indicate wind direction."
     )
     wp.fig.text(0.02, 0.125, lbl, va="bottom")
@@ -349,26 +349,7 @@ def _make_plot(
     return wp.fig
 
 
-def windrose(
-    station,
-    database="asos",
-    months=np.arange(1, 13),
-    hours=np.arange(0, 24),
-    sts=datetime(1970, 1, 1),
-    ets=datetime(2050, 1, 1),
-    units="mph",
-    nsector=36,
-    justdata=False,
-    rmax=None,
-    sname=None,
-    sknt=None,
-    drct=None,
-    valid=None,
-    level=None,
-    bins=None,
-    tzname=None,
-    **kwargs,
-):
+def windrose(station, **kwargs):
     """Utility function that generates a windrose plot
 
     Args:
@@ -378,60 +359,49 @@ def windrose(
       hours (list,optional): optional list of hours to limit plot to
       sts (datetime,optional): start datetime
       ets (datetime,optional): end datetime
-      units (str,optional): units to plot values as
+      units (str,optional): units to plot values as, default to `mph`.
       nsector (int,optional): number of bins to devide the windrose into
       justdata (boolean,optional): if True, write out the data only
       sname (str,optional): The name of this station, if not specified it will
         default to the ((`station`)) identifier
       sknt (list,optional): A list of wind speeds in knots already generated
       drct (list,optional): A list of wind directions (deg N) already generated
-      valid (list,optional): A list of valid datetimes (with tzinfo set)
+      valid (list,optional): A list of valid datetimes (with tzinfo set), used
+        in the case of providing sknt and drct.
       level (int,optional): In case of RAOB, which level interests us (hPa)
       bins (list,optional): bins to use for the wind speed
       tzname (str,optional): Time zone to use for the plot.
       cmap (cmap,optional): Matplotlib colormap to pass to barplot.
+      limit_by_doy (bool,optional): Use the `sts` and `ets` to define a period
+        of days each year to limit the data by. Default `false`.
 
     Returns:
       matplotlib.Figure instance or textdata
     """
-    monthinfo = _get_timeinfo(months, "month", 12, tzname)
-    hourinfo = _get_timeinfo(hours, "hour", 24, tzname)
-    wu = WINDUNITS[units]
+    wu = WINDUNITS[kwargs.get("units", "mph")]
+    if kwargs.get("sts") is None:
+        kwargs["sts"] = datetime(1970, 1, 1)
+    if kwargs.get("ets") is None:
+        kwargs["ets"] = datetime(2050, 1, 1)
+    sknt = kwargs.get("sknt")
+    drct = kwargs.get("drct")
     if sknt is None or drct is None:
-        df = _get_data(station, database, sts, ets, monthinfo, hourinfo, level)
+        df = _get_data(
+            station,
+            **kwargs,
+        )
     else:
-        df = pd.DataFrame({"sknt": sknt, "drct": drct, "valid": valid})
+        df = pd.DataFrame(
+            {"sknt": sknt, "drct": drct, "valid": kwargs.get("valid")}
+        )
     # Make sure our bins have units
+    bins = kwargs.get("bins")
     if not hasattr(bins, "units") and bins:
-        bins = bins * wu["units"]
+        kwargs["bins"] = bins * wu["units"]
     # Convert wind speed into the units we want here
     if df["sknt"].max() > 0:
         df["speed"] = (df["sknt"].values * mpunits("knots")).to(wu["units"]).m
-    if justdata:
-        return _make_textresult(
-            station,
-            df,
-            units,
-            nsector,
-            sname,
-            monthinfo,
-            hourinfo,
-            level,
-            bins,
-            tzname,
-        )
+    if kwargs.get("justdata", False):
+        return _make_textresult(station, df, **kwargs)
 
-    return _make_plot(
-        station,
-        df,
-        units,
-        nsector,
-        rmax,
-        hours,
-        months,
-        sname,
-        level,
-        bins,
-        tzname,
-        **kwargs,
-    )
+    return _make_plot(station, df, **kwargs)
