8d462948eb9493a42e452620a0297e61367f26a8
akrherz <akrherz@iastate.edu>
2022-11-23 07:34:00 -0600

    refactor util.get_file_name testing helper

--- tests/nws/test_gini.py
+++ tests/nws/test_gini.py
@@ -1,6 +1,6 @@
 """Test GINI"""
 
-from pyiem.util import get_test_file
+from pyiem.util import get_test_filepath
 from pyiem.nws import gini
 
 
@@ -12,7 +12,9 @@ def test_getirramp():
 
 def test_conus():
     """Test processing a national product"""
-    sat = gini.GINIZFile(get_test_file("TIGN02", fponly=True))
+    fp = get_test_filepath("TIGN02")
+    with open(fp, "rb") as fh:
+        sat = gini.GINIZFile(fh)
     assert sat.archive_filename() == "GOES_SUPER_IR_201509281745.png"
     assert sat.awips_grid() == 0
     assert sat.metadata["map_projection"] == 5
@@ -20,7 +22,9 @@ def test_conus():
 
 def test_gini():
     """check GINI Processing of Goes East VIS parsing"""
-    sat = gini.GINIZFile(get_test_file("TIGH05", fponly=True))
+    fp = get_test_filepath("TIGH05")
+    with open(fp, "rb") as fh:
+        sat = gini.GINIZFile(fh)
     assert sat.archive_filename() == "GOES_HI_WV_201507161745.png"
     assert str(sat) == "TIGH05 KNES 161745 Line Size: 560 Num Lines: 520"
     assert sat.awips_grid() == 208
