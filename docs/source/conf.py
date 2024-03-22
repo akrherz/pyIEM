2b2b92e24ee6fd1b5b848425e5f4b579da9d7c36
akrherz <akrherz@iastate.edu>
2024-03-22 12:18:51 -0500

    ci: logic improvements with docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ needs_sphinx = "1.3"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+# napoleon is for google/numpy style docstrings
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
