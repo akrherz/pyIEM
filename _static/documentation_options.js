6c4e4fdecbc67aa679f1f1f7d04794062c135fe2
akrherz <akrherz@iastate.edu>
2026-04-01 03:09:58 +0000

    Deploying to gh-pages from @ akrherz/pyIEM@f1dd3695cd1385a62bb6095bd4accb274dac2e58 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.26.0.post83+g754f7da13',
+    VERSION: '1.26.0.post85+gf1dd3695c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
