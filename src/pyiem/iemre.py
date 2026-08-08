eda275a96c62a24d1e674c622dec6284ae398f48
akrherz <akrherz@iastate.edu>
2026-08-07 23:48:11 -0500

    💚 Apply suggested better check

--- src/pyiem/iemre.py
+++ src/pyiem/iemre.py
@@ -353,7 +353,9 @@ def grb2iemre(grb, resampling=None, domain: str = "conus") -> np.ndarray:
             dy = -dy
         # Ugly situation here for data that starts at 0° and goes to 360°
         vals = grb.values
-        if llx == 0:
+        if np.isclose(llx, 0.0) and np.isclose(
+            vals.shape[1] * abs(dx), 360.0, atol=1
+        ):
             vals = np.roll(vals, vals.shape[1] // 2, axis=1)
             llx = -180.0
         aff = Affine(
